5913d66a13b60b26fc51b005ddb05bcb709d44fc
lispro <lispro06@gmail.com>
2010-11-19 14:02:02 +0900

    ex 1.3 update by lispro

--- ch01/1.3/ex-1-3-lispro.scm
+++ ch01/1.3/ex-1-3-lispro.scm
@@ -173,3 +173,33 @@
 (newline)
 (filtered-accumulate + 0 sq 2 inc 10 prime?)
 (filtered-accumulate-iter * 1 identity 1 inc 10 gcd1?)
+
+;ex 1.34
+(define (f g)
+  (g 2))
+(f sq)
+(f (lambda (z) (* z (+ z 1))))
+;(f f) is error ; argument must be procedure (e. g. + - * or sq, etc)
+
+;ex 1.35
+; funtion fixed point of f(x) is x at f(x) =x .
+; Thus, x is fixed point at '1 + 1/x = x' 
+; 1 + 1/x = x ====> x^2 = x + 1
+; At this, x^2 = x + 1 is fomular for golden ratio(We can check 1.2.2(50p)
+;Therefore, golden ratio is fixed point of 'x |-> 1 + 1/x'
+
+; fixed-point procedure calculation
+(define tolerance 0.0001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2) ) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+         next
+         (try next))))
+  (try first-guess)
+  )
+;result
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+
