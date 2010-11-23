0984c11661fcfc04077f2f4bb27038035044c106
byulparan <byulparan@gmail.com>
2010-11-24 00:25:12 +0900

    ex-1-3 byulparan

--- ch01/1.3/ex-1-3-byulparan.scm
+++ ch01/1.3/ex-1-3-byulparan.scm
@@ -17,15 +17,14 @@
   (* (sum f (+ a (/ dx 2.0)) add-dx b) dx))
 
 
-
 (define (simpson f a b n)
   (define h (/ (- b a) n))
   (define (y k)
     (f (+ a (* k h))))
   (define (term k)
     (* (y k) (cond ((or (= k 0) (= k n)) 1)
-		   ((odd? k) 4)
-		   (else 2))))
+		   ((odd? k) 4.0)
+		   (else 2.0))))
   (* (/ h 3.0) (sum term 0 inc n)))
 
 
