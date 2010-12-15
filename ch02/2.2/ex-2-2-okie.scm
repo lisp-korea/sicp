a9ee6301b6ac20b8daee01c1ecd7d3d0b048ee14
okie <jimmy.sh.yang@gmail.com>
2010-12-15 10:26:56 +0900

    fix 2.2.24 diagram

--- ch02/2.2/ex-2-2-okie.scm
+++ ch02/2.2/ex-2-2-okie.scm
@@ -225,27 +225,44 @@
 (list 1 (list 2 (list 3 4)))
 
 ; list type
-+---------------+     +---+--+     +---+--+     +---+--+     +---+--+
-| (1 (2 (3 4))) | --> |   |  | --> |   |  | --> |   |  | --> |   |  |
-+---------------+     +---+--+     +---+--+     +---+--+     +---+--+
-                        |            |            |            |
-                        |            |            |            |
-                        v            v            v            v
-                      +---+        +---+        +---+        +---+
-                      | 1 |        | 2 |        | 3 |        | 4 |
-                      +---+        +---+        +---+        +---+
-
+;; +---------------+     +---+--+     +---+--+
+;; | (1 (2 (3 4))) | --> |   |  | --> |   |  |
+;; +---------------+     +---+--+     +---+--+
+;;                         |            |
+;;                         |            |
+;;                         v            v
+;;                       +---+        +---+--+     +---+--+
+;;                       | 1 |        |   |  | --> |   |  |
+;;                       +---+        +---+--+     +---+--+
+;;                                      |            |
+;;                                      |            |
+;;                                      v            v
+;;                                    +---+        +---+--+     +---+--+
+;;                                    | 2 |        |   |  | --> |   |  |
+;;                                    +---+        +---+--+     +---+--+
+;;                                                   |            |
+;;                                                   |            |
+;;                                                   v            v
+;;                                                 +---+        +---+--+
+;;                                                 | 3 |        |   |  |
+;;                                                 +---+        +---+--+
+;;                                                                |
+;;                                                                |
+;;                                                                v
+;;                                                              +---+
+;;                                                              | 4 |
+;;                                                              +---+
 
 ; tree type?
-+---------------+     +-----------+     +-------+     +---+
-| (1 (2 (3 4))) | --> | (2 (3 4)) | --> | (3 4) | --> | 4 |
-+---------------+     +-----------+     +-------+     +---+
-  |                     |                 |
-  |                     |                 |
-  v                     v                 v
-+---------------+     +-----------+     +-------+
-|       1       |     |     2     |     |   3   |
-+---------------+     +-----------+     +-------+
+;; +---------------+     +-----------+     +-------+     +---+
+;; | (1 (2 (3 4))) | --> | (2 (3 4)) | --> | (3 4) | --> | 4 |
+;; +---------------+     +-----------+     +-------+     +---+
+;;   |                     |                 |
+;;   |                     |                 |
+;;   v                     v                 v
+;; +---------------+     +-----------+     +-------+
+;; |       1       |     |     2     |     |   3   |
+;; +---------------+     +-----------+     +-------+
 
 ; ex 2.25
 
@@ -448,3 +465,86 @@
 (subsets '(1 2 3))
 ; 1. () (3) (2) (2 3)
 ; 2. (1) (1 2) (1 3) (1 2 3)
+
+; 2.2.3 conventional interface
+
+(define z (list (list (list 1 2) (list 3 4)) 5 6))
+
+(define (square x)
+  (* x x))
+
+(define (sum-odd-squares tree)
+  (cond ((null? tree) 0)
+        ((not (pair? tree))
+         (if (odd? tree) (square tree) 0))
+        (else (+ (sum-odd-squares (car tree))
+                 (sum-odd-squares (cdr tree))))))
+
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+
+(define (even-fibs n)
+  (define (next k)
+    (if (> k n)
+        '()
+        (let ((f (fib k)))
+          (if (even? f)
+              (cons f (next (+ k 1)))
+              (next (+ k 1))))))
+  (next 0))
+              
+(map square (list 1 2 3 4 5))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) '())
+        ((predicate (car sequence))
+         (cons (car sequence)
+              (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(filter odd? (list 1 2 3 4 5))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(accumulate + 0 (list 1 2 3 4 5))
+
+(accumulate * 1 (list 1 2 3 4 5))
+
+(accumulate cons '() (list 1 2 3 4 5))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      '()
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(enumerate-interval 2 7)
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+(enumerate-tree (list 1 (list 2 (list 3 4)) 5))
+
+(define (sum-odd-squares tree)
+  (accumulate +
+              0
+              (map square
+                   (filter odd?
+                           (enumerate-tree tree)))))
+
+(define (even-fibs n)
+  (accumulate cons
+              '()
+              (filter even?
+                      (map fib
+                           (enumerate-interval 0 n)))))
