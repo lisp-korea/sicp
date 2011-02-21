137c7b5f9f41e1ba50f08781bec94125752abb74
lispro <lispro06@gmail.com>
2011-02-21 15:17:13 +0900

    ex 3.2 lispro

--- ch03/3.2/ex-3-2-lispro.scm
+++ ch03/3.2/ex-3-2-lispro.scm
@@ -6,7 +6,7 @@
 ;             -  |
 ;              |  -
 ;              v   |
-;             −−-
+;             しし-
 ;             |
 ;             v
 ;        Parameters : n
@@ -36,7 +36,7 @@
 ;             -    |         |    |
 ;              |   |         |    |
 ;              v   |         v    |
-;             −−-        −−---
+;             しし-        しし---
 ;             |             |
 ;             v              ---->   Pameters : product
 ;        Parameters : n              Counter max-count
@@ -57,3 +57,136 @@
 ;                   Product
 ;		    (fact-iter (* counter product)
 ;		    (+ counter 1) max-count))
+
+;ex 3.10
+;
+;
+;           --------------------------------------------------------
+;          | make-withdraw --------------------------               |
+;          | W1----                                  |              |
+;           -------|--------------^- ----------------|------^-------
+;                  |              |                  |      |
+;                  |          ------------------     v      |
+;                --       E1 |initial-amount:100|   しし----
+;               |     E2      --------^---------    |
+;               |      |              |             |
+;               |    --               |             |
+;               |   |    -----------  |             |
+;               |    -->|balance:100|-              V
+;               |        ----^------         Parameters : initial-amount
+;               |            |               body :
+;             しし-----------                 ((lambda (balance)
+;	      |				       (lambda (amount)
+;	       -			        (if (>=balance amount)
+;		|				  (begin (set! balace (- balace amount))
+;		v				      balance)
+;	parameters : amount			  "Insufficient funds"))) initial-amount)
+;       body :
+;       (if (>= balance amount)
+;        (begin (set! balance (- balance amount))
+;         balance)
+;       "Insufficient funds")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;(W1 50)
+;           --------------------------------------------------------
+;          | make-withdraw --------------------------               |
+;          | W1----                                  |              |
+;           -------|--------------^- ----------------|------^-------
+;                  |              |                  |      |
+;                  |          ------------------     v      |
+;                --       E1 |initial-amount:100|   しし----
+;               |     E2      --------^---------    |
+;               |      |              |             |
+;               |    --               |             |
+;               |   |    -----------  |             |
+;               |    -->|balance:100|-              V
+;               |        ----^---^--					Parameters : initial-amount
+;               |            |   |					body :
+;             しし-----------    |					 ((lambda (balance)
+;	      |			 ---------				  (lambda (amount)
+;	       -		|amount:50|				   (if (>=balance amount)
+;		|		 ---------				    (begin (set! balace (- balace amount))
+;		v		if( (>=balance amount)			      balance)
+;				 (begin (set! balance (- balace amount))  "Insufficient funds"))) initial-amount)
+;	parameters : amount        balance)
+;       body :			 "Insufficient funds")
+;       (if (>= balance amount)  
+;        (begin (set! balance (- balance amount))
+;         balance)
+;       "Insufficient funds")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;           --------------------------------------------------------
+;          | make-withdraw --------------------------               |
+;          | W1----                                  |              |
+;           -------|--------------^- ----------------|------^-------
+;                  |              |                  |      |
+;                  |          ------------------     v      |
+;                --       E1 |initial-amount:100|   しし----
+;               |     E2      --------^---------    |
+;               |      |              |             |
+;               |    --               |             |
+;               |   |     ----------  |             |
+;               |    --> |balance:50|-              V
+;               |         ---^------         Parameters : initial-amount
+;               |            |               body :
+;             しし-----------                 ((lambda (balance)
+;	      |				       (lambda (amount)
+;	       -			        (if (>=balance amount)
+;		|				  (begin (set! balace (- balace amount))
+;		v				      balance)
+;	parameters : amount			  "Insufficient funds"))) initial-amount)
+;       body :
+;       (if (>= balance amount)
+;        (begin (set! balance (- balance amount))
+;         balance)
+;       "Insufficient funds")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;           ----------------------------------------------------------------------------------------------
+;          | make-withdraw ----------------------------------------------------------------------         |
+;          | W1----                                                                              |        |
+;          | W2----|---------------------------------                                            |        |
+;           -------|--------------^- ----------------|------^-----------------------^------------|----^---
+;                  |              |                  |E4    |                       |            |    |
+;                  |          ------------------     ||   -----------       ------------------   |    |
+;                --       E1 |initial-amount:100|    | ->|balance:100| E3->|initial-amount:100|  |    |
+;               |     E2      --------^---------     |    -----------       --------^---------   v    |
+;               |      |              |              |      |    |                  |           しし--
+;               |    --               |             しし----      ------------------             |
+;               |   |    ----------   |              |             Parameters : initial-amount<--
+;               |    -->|balance:50|--               V             body :
+;               |        ---^------  Parameters : amount                     (lambda (balance)
+;               |           |        body :                                   (lambda (amount)
+;             しし----------          (if (>= balance amount)                  (if (>=balance amount)
+;	      |			       (begin (set! balace (- balace amount))   (begin (set! balace (- balace amount))
+;	       -			balance)                                 balance)
+;		|			"Insufficient funds")	             "Insufficient funds"))) initial-amount)
+;		v				      
+;	parameters : amount			  
+;       body :
+;       (if (>= balance amount)
+;        (begin (set! balance (- balance amount))
+;         balance)
+;       "Insufficient funds")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (make-withdraw initial-amount)
+  (let ((balance initial-amount))
+    (lambda (amount)
+      (cond ((number? amount)
+      (if (>= balance amount)
+          (begin (set! balance (- balance amount))
+                 balance)
+          "Insufficient funds"))
+            (else (display initial-amount)
+                  (display "  ")
+                  (display balance)
+                  (newline)))))) 
+
+(define W1 (make-withdraw 100))
+(W1 'd)
+(W1 50)
+(W1 'd)
+(newline)
+(define W2 (make-withdraw 100))
+(W2 'd)
