2c650612a1df77313578fccde3ffce675e05b6c6
byulparan <byulparan@gmail.com>
2011-03-02 04:20:12 +0900

    ex-3-3 별파란

--- ch03/3.3/ex-3-3-byulparan.scm
+++ ch03/3.3/ex-3-3-byulparan.scm
@@ -138,9 +138,137 @@ z2
 
 ;; 연습문제 3.18
 
+(define (make-cycle list)
+  (set-cdr! (last-pair list) list)
+  list)
+
+(let ((a (make-cycle '(1 2 3))))
+  (eq? a (cdddr a)))
+
+(define (find-cycle list)
+  (let ((copy '()))
+    (define (inner-find-cycle x)
+      (cond ((null? x) #f)
+	    ((find-item x copy) #t)
+	    (else (begin
+		    (set! copy (cons x copy))
+		    (inner-find-cycle (cdr x))))))
+    (inner-find-cycle (cdr list))))
+
+(find-cycle '(1 2 3 4))
+(find-cycle (make-cycle '(1 2 3 4)))
+(find-cycle (cons 'q (make-cycle '(1 2 3 4))))
+
+;; 연습문제 3.19
+;; Floyd's idea:
+ (define (contains-cycle? lst) 
+   (define (safe-cdr l) 
+     (if (pair? l) 
+         (cdr l) 
+         '())) 
+   (define (iter a b) 
+     (cond ((not (pair? a)) #f) 
+           ((not (pair? b)) #f) 
+           ((eq? a b) #t) 
+           ((eq? a (safe-cdr b)) #t) 
+           (else (iter (safe-cdr a) (safe-cdr (safe-cdr b)))))) 
+   (iter (safe-cdr lst) (safe-cdr (safe-cdr lst))))
+
+(contains-cycle? (make-cycle '(1 2 3 4)))
+
+;; 연습문제 3.20
+
+(define (cons2 x y)
+  (define (set-x! v) (set! x v))
+  (define (set-y! v) (set! y v))
+  (define (dispatch m)
+    (cond ((eq? m 'car) x)
+	  ((eq? m 'cdr) y)
+	  ((eq? m 'set-car!) set-x!)
+	  ((eq? m 'set-cdr!) set-y!)
+	  (else (error "Undefined operation --CONS" m))))
+  dispatch)
+
+(define (car2 z)
+  (z 'car))
+
+(define (cdr2 z)
+  (z 'cdr))
+
+(define (set-car2! z new-value)
+  ((z 'set-car!) new-value)
+  z)
+
+(define (set-cdr2! z new-value)
+  ((z 'set-cdr!) new-value)
+  z)
+
+(define x (cons2 1 2))
+(define z (cons2 x x))
+(set-car2! (cdr2 z) 17)
+
+(car2 x)
 
 
+;; 3.3.2 큐
 
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item) (set-car! queue item))
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
+
+(define (empty-queue? queue)
+  (null? (front-ptr queue)))
+
+(define (make-queue)
+  (cons '() '()))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue) (begin
+				  (set-front-ptr! queue new-pair)
+				  (set-rear-ptr! queue new-pair)
+				  queue))
+	  (else
+	   (set-cdr! (rear-ptr queue) new-pair)
+	   (set-rear-ptr! queue new-pair)
+	   queue))))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue) (error "DELETE! called with an empty queue" queue))
+	(else
+	 (set-front-ptr! queue (cdr (front-ptr queue)))
+	 queue)))
+
+
+;; 연습문제 3.21
+
+(define (error message err)
+  (display message)
+  (display " ") (display err) (display "\n"))
+
+(define q1 (make-queue))
+
+(insert-queue! q1 'a)
+(insert-queue! q1 'b)
+(delete-queue! q1)
+(delete-queue! q1)
+(empty-queue? q1)
+
+(define (print-queue queue)
+  (front-ptr queue))
+
+(print-queue q1)
+	   
+	   
+
+;; 연습문제 3.22
+;; 
 
 
 
