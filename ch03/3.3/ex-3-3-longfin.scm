3e9e7918c9c9ec4ff8b17c5a98c068ecac9f5b0d
longfin <longfinfunnel@gmail.com>
2011-03-04 00:31:16 +0900

    -Solve to ex 3.19

--- ch03/3.3/ex-3-3-longfin.scm
+++ ch03/3.3/ex-3-3-longfin.scm
@@ -134,3 +134,165 @@
 ;; w : (d c b a)
 
 
+;; Sharing and identity
+
+(define x (list 'a 'b))
+;; [[a] [+]]
+;;        +-[[b] [nil]]
+
+(define z1 (cons x x))
+
+;; [[+] [+]]
+;;   +   +--+ 
+;;   +-----+- x
+
+(define z2 (cons (list 'a 'b) (list 'a 'b)))
+;; [[+] [+]]
+;;   +   +-[[a] [+]]
+;;   +              +-[[b] [nil]]
+;;   +-[[a] [+]]
+;;              +-[[b] [nil]]
+
+(define (set-to-wow! x)
+  (set-car! (car x) 'wow)
+  x)
+(set-to-wow! z1)
+(set-to-wow! z2)
+
+;; ex 3.15
+
+;; z1
+;; [[+] [+]]
+;;   +   +--+ 
+;;   +-----+- [[wow] [+]]
+;;                                 +-[[b] [nil]]
+
+;; z2
+;; [[+] [+]]
+;;   +   +-[[a] [+]]
+;;   +              +-[[b] [nil]]
+;;   +-[[wow] [+]]
+;;                    +-[[b] [nil]]
+
+;; ex 3.16
+(define (count-pairs x)
+  (if (not (pair? x))
+      0
+      (+ (count-pairs (car x))
+	 (count-pairs (cdr x))
+	 1)))
+
+(define count3 '(1 2 3))
+;; [[1] [+]]
+;;        +-[[2] [+]]
+;;                    +-[[3] [nil]]
+(count-pairs count3)
+
+(define p (cons 1 2))
+;; [[1] [+]]
+;;        +-[[p] [nil]]
+(define count4 (cons p (cons p 4)))
+;; [[+] [+]]
+;;   +   +-[[+] [4]]
+;;   +          +   
+;;   +          +-[[1] [2]
+;;   +-[[1] [2]]
+(count-pairs count4)
+
+(define p2 (cons p p))
+;; [[+] [+]]
+;;   ++ +-[[1] [2]]
+(define count7 (cons p2 p2))
+;; [[+] [+]]
+;;   +++
+;;        +-[[+] [+]]
+;;               +++-[[1] [2]]
+(count-pairs count7)
+
+(define counti (cons p p))
+(set-cdr! counti counti)
+
+;; [[+] [+]] => counti
+;;   +   +-counti
+;;   +-[[1] [2]]
+
+;; ex 3.17
+
+(define (contains? list x)
+  (cond
+   ((null? list) #f)
+   ((eq? x (car list)) #t)
+   (else (contains? (cdr list) x))))
+(define (count-pairs x)
+  (let ((founded '()))
+    (define (count-pairs-inner x)
+      (cond
+       ((not (pair? x)) 0)
+       ((contains? founded x) 0)
+       (else
+	(set! founded (cons x founded))
+	(+
+	 (count-pairs-inner (car x))
+	 (count-pairs-inner (cdr x))
+	 1))))
+    (count-pairs-inner x)))
+
+;; ex 3.18
+
+(define (cycle? x)
+  (let ((founded '()))
+    (define (inner x)
+      (cond
+       ((not (pair? x)) #f)
+       ((null? x) #f)
+       ((contains? founded x) #t)
+       (else
+	(set! founded (cons x founded))
+	(inner (cdr x)))))
+    (inner x)))
+
+(define x (cons 1 2))
+(cycle? x)
+;; #f
+(set-cdr! x x)
+(cycle? x)
+;; #t
+
+(define y (cons 3 x))
+(set-cdr! x y)
+(cycle? x)
+;;#t
+
+(set-car! x x)
+(cycle? x)
+;;#t
+
+(define z (cons x 3))
+(set-car! x z)
+(cycle? x)
+;;#t
+
+;; ex 3.19
+
+(define (cycle? x)
+  (define (safe-cdr x)
+    (if (pair? x)
+	(cdr x)
+	'()))
+  (define (iter a b)
+    (cond
+     ((not (pair? a)) #f)
+     ((not (pair? b)) #f)
+     ((eq? a b) #t)
+     ((eq? a (safe-cdr b)) #t)
+     (else (iter (safe-cdr a)
+		 (safe-cdr (safe-cdr b))))))
+  (iter (safe-cdr x) (safe-cdr (safe-cdr x))))
+
+;; 2nd and 4th
+;; 2nd and 5th
+
+;; 3nd and 6th
+;; 3nd and 7th
+
+;; ...
