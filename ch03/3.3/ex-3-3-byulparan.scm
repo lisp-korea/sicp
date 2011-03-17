a5004c95c570293265ca3a01f6688323852b0bf6
byulparan <byulparan@gmail.com>
2011-03-18 04:46:10 +0900

    3.3.4 디지털 회로 시뮬레이터 추가. byulparan

--- ch03/3.3/ex-3-3-byulparan.scm
+++ ch03/3.3/ex-3-3-byulparan.scm
@@ -683,6 +683,9 @@ z2
   (add-action! a2 and-action-procedure)
   'ok)
 
+(define (logical-and s1 s2)
+  (cond ((and (= s1 1) (= s2 1)) 1)
+	(else 0)))
 
 ;; 연습문제 3.28
 
@@ -730,8 +733,167 @@ z2
 	   (ripple-carry-adder (cdr a) (cdr b) (cdr s) c-out)))))
 
 
-  
 
+;; 줄만들기
+
+(define (make-wire)
+  (let ((signal-value 0) (action-procedures '()))
+    (define (set-my-signal! new-value)
+      (if (not (= signal-value new-value))
+	  (begin (set! signal-value new-value)
+		 (call-each action-procedures))
+	  'done))
+    (define (accept-action-procedure! proc)
+      (set! action-procedures (cons proc action-procedures))
+      (proc)
+      )
+
+    (define (get-action-procedures)
+      action-procedures)
+
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+	    ((eq? m 'set-signal!) set-my-signal!)
+	    ((eq? m 'add-action!) accept-action-procedure!)
+	    ((eq? m 'get-action) get-action-procedures)
+	    (else
+	     (error "Unknown operation --WIRE" m))))
+    dispatch))
+
+(define (call-each procedures)
+  (if (null? procedures) 'done
+      (begin
+	((car procedures))
+	(call-each (cdr procedures)))))
+
+(define (get-signal wire)
+  (wire 'get-signal))
+
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
+
+
+;; 시간표
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+		  action
+		  the-agenda))
+
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-item (first-agenda-item the-agenda)))
+	(first-item)
+	(remove-first-agenda-item! the-agenda)
+	(propagate))))
+
+
+;; 시뮬레이션 해보기
+(define (probe name wire)
+  (add-action! wire
+	       (lambda ()
+		 (newline)
+		 (display name)
+		 (display " ")
+		 (display (current-time the-agenda))
+		 (display "  new-value = ")
+		 (display (get-signal wire)))))
+
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+(define input-1 (make-wire))
+(define input-2 (make-wire))
+(define sum (make-wire))
+(define carry (make-wire))
+
+;; 연습문제 3.31
+
+;; 
+
+;; 시간표 만들기
+
+(define (make-time-segment time queue)
+  (cons time queue))
+
+(define (segment-time s)
+  (car s))
+
+(define (segment-queue s)
+  (cdr s))
+
+(define (make-agenda)
+  (list 0))
+
+(define (current-time agenda)
+  (car agenda))
+
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+
+(define (segments agenda)
+  (cdr agenda))
+
+(define (set-segments! agenda segments)
+  (set-cdr! agenda segments))
+
+(define (first-segment agenda)
+  (car (segments agenda)))
+
+(define (rest-segments agenda)
+  (cdr (segments agenda)))
+
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+(define (add-to-agenda! time action agenda)
+  (define (belongs-before? segments)
+    (or (null? segments)
+	(< time (segment-time (car segments)))))
+  
+  (define (make-new-time-segment time action)
+    (let ((q (make-queue)))
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  
+  (define (add-to-segments! segments)
+    (if (= (segment-time (car segments)) time)
+	(insert-queue! (segment-queue (car segments))
+		       action)
+	(let ((rest (cdr segments)))
+	  (if (belongs-before? rest)
+	      (set-cdr!
+	       segments (cons (make-new-time-segment time action)
+			      (cdr segments)))
+	      (add-to-segments! rest)))))
+  
+  (let ((segments (segments agenda)))
+    (if (belongs-before? segments)
+	(set-segments!
+	 agenda
+	 (cons (make-new-time-segment time action) segments))
+	(add-to-segments! segments))))
+
+(define (remove-first-agenda-item! agenda)
+  (let ((q (segment-queue (first-segment agenda))))
+    (delete-queue! q)
+    (if (empty-queue? q)
+	(set-segments! agenda (rest-segments agenda)))))
+
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+      (error "Agenda is empty -- FIRSTAGENDA-ITEM")
+      (let ((first-seg (first-segment agenda)))
+	(set-current-time! agenda (segment-time first-seg))
+	(front-queue (segment-queue first-seg)))))
+
+      
 
 
 
