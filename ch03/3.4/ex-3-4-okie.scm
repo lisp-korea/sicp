f0b6b980df80e8a46894f0083348ae64ee905187
lisp-korea <jimmy.sh.yang@gmail.com>
2011-03-26 08:17:21 +0900

    ex-3-4

--- ch03/3.4/ex-3-4-okie.scm
+++ ch03/3.4/ex-3-4-okie.scm
@@ -47,6 +47,28 @@
     (for-each thread-wait list-of-threads)
     balance))
 
+;; [peter, paul, mary] -> 45
+;; [peter, mary, paul] -> 35
+;; [mary, peter, paul] -> 40
+;; [mary, paul, peter] -> 40
+;; [paul, peter, mary] -> 45
+;; [paul, mary, peter] -> 50
+
+
+;; PLT Scheme implementation
+(define (parallel-execute . thunks)
+  (for-each thread thunks))
+
+(define (make-serializer)
+  (let ((mutex (make-semaphore 1)))
+    (lambda (p)
+      (define (serialized-p . args)
+        (semaphore-wait mutex)
+        (let ((val (apply p args)))
+          (semaphore-post mutex)
+          val))
+      serialized-p)))
+
 ;; 3.4.2
 
 (define x 10)
@@ -156,3 +178,215 @@
       dispatch)))
 
 ;; not safe because deposit always follows withdraw
+
+;; 여러자원을 함께 쓰는 문제
+
+(define (exchange account1 account2)
+  (let ((difference (- (account1 'balance)
+                       (account2 'balance))))
+    ((account1 'withdraw) difference)
+    ((account2 'deposit) difference)))
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((protected (make-serializer)))
+    (let ((protected-withdraw (protected withdraw))
+          (protected-deposit (protected deposit)))
+      (define (dispatch m)
+        (cond ((eq? m 'withdraw) protected-withdraw)
+              ((eq? m 'deposit) protected-deposit)
+              ((eq? m 'balance) balance)
+              (else (error "Unknown request -- MAKE-ACCOUNT"
+                           m))))
+      dispatch)))
+
+(define (make-account-and-serializer balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((balance-serializer (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) withdraw)
+            ((eq? m 'deposit) deposit)
+            ((eq? m 'balance) balance)
+            ((eq? m 'serializer) balance-serializer)
+            (else (error "Unknown request -- MAKE-ACCOUNT"
+                         m))))
+    dispatch))
+
+(define (deposit account amount)
+  (let ((s (account 'serializer))
+        (d (account 'deposit)))
+    ((s d) amount)))
+
+(define (serialized-exchange account1 account2)
+  (let ((serializer1 (account1 'serializer))
+        (serializer2 (account2 'serializer)))
+    ((serializer1 (serializer2 exchange))
+     account1
+     account2)))
+
+;; ex 3.43
+
+;; refer to longfin's solution
+
+
+;; ex 3.44
+(define (transfer from-account to-account account)
+  ((from-account 'withdraw) amount)
+  ((to-account 'deposit) amount))
+
+;; ex 3.45
+(define (make-account-and-serializer balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((balance-serializer (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) (balance-serializer withdraw))
+            ((eq? m 'deposit) (balance-serializer deposit))
+            ((eq? m 'balance) balance)
+            ((eq? m 'serializer) balance-serializer)
+            (else (error "Unknown request -- MAKE-ACCOUNT"
+                         m))))
+    dispatch))
+
+(define (deposit account amount)
+  ((account 'deposit) amount))
+
+;; not works in inter-account
+
+;; 줄 세우개 만들기
+(define (make-serializer)
+  (let ((mutex (make-mutex)))
+    (lambda (p)
+      (define (serialized-p . args)
+        (mutex 'acquire)
+        (let ((val (apply p args)))
+          (mutex 'release)
+          val))
+      serialized-p)))
+
+(define (make-mutex)
+  (let ((cell (list false)))
+    (define (the-mutex m)
+      (cond ((eq? m 'acquire)
+             (if (test-and-set! cell)
+                 (the-mutex 'acquire)))
+            ((eq? m 'release) (clear! cell))))
+    the-mutex))
+
+(define (clear! cell)
+  (set-car! cell false))
+
+(define (test-and-set! cell)
+  (if (car cell)
+      true
+      (begin (set-car! cell true)
+             false)))
+
+
+;; 3.46
+
+;; test와 set 중간에 값이 바뀔수 있다.
+
+;; 3.47
+
+;; a.
+(define (make-semaphore-mtx maximal)
+  (let ((count maximal)
+        (mutex (make-mutex)))
+    (define (the-sema m)
+      (cond ((eq? m 'release)
+              (mutex 'acquire)
+              (unless (= count maximal)
+                (set! count (+ 1 count)))
+              (mutex 'release))
+            ((eq? m 'acquire)
+              (mutex 'acquire)
+              (cond
+                ((> count 0)
+                  (set! count (- count 1))
+                  (mutex 'release))
+                (else
+                  (mutex 'release)
+                  (the-sema 'acquire))))
+            (else
+              (error "Unknown request -- " m))))
+    the-sema))
+
+;; b.
+(define (loop-test-and-set! cell)
+  (if (test-and-set! cell)
+    (loop-test-and-set! cell)
+    '()))
+
+(define (make-semaphore-ts maximal)
+  (let ((count maximal)
+        (guard (cons #f '())))
+    (define (the-sema m)
+      (cond ((eq? m 'release)
+              (loop-test-and-set! guard)
+                (unless (= count maximal)
+                  (set! count (+ 1 count)))
+              (clear! guard))
+            ((eq? m 'acquire)
+              (cond
+                (loop-test-and-set! guard)
+                ((> count 0)
+                  (set! count (- count 1))
+                  (clear! guard))
+                (else
+                  (clear! guard)
+                  (the-sema 'acquire))))
+            (else
+              (error "Unknown request -- " m))))
+    the-sema))
+
+;; deadlock
+
+;; ex 3.48
+(define (make-account number balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((balance-serializer (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) withdraw)
+            ((eq? m 'deposit) deposit)
+            ((eq? m 'number) number)
+            ((eq? m 'balance) balance)
+            ((eq? m 'serializer) balance-serializer)
+            (else (error "Unknown request -- MAKE-ACCOUNT"
+                         m))))
+    dispatch))
+(define (serialized-exchange account1 account2)
+  (let ((serializer1 (account1 'serializer))
+        (serializer2 (account2 'serializer)))
+    (if (< (account1 'number) (account2 'number))
+      ((serializer2 (serializer1 exchange))
+        account1 account2)
+      ((serializer1 (serializer2 exchange))
+        account1 account2))))
