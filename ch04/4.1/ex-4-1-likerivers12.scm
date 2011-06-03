34bb728c3173e58206f2cebd6675d7ccfbea1a23
likerivers12 <likerivers12@gmail.com>
2011-06-03 22:05:50 +0900

    ex 4.4

--- ch04/4.1/ex-4-1-likerivers12.scm
+++ ch04/4.1/ex-4-1-likerivers12.scm
@@ -1,3 +1,7 @@
+
+;; racket -l r5rs/run
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Ch 4 언어를 처리하는 기법
 
@@ -63,7 +67,7 @@
 ;;    실제 프로시저에 인자를 주고 계산하는 일은 apply가 맡아서 한다.
 
 ;;; eval 정의
-(define (eval exp env)
+(define (my-eval exp env)
   (cond ((self-evaluating? exp) exp)
 	((variable? exp) (lookup-variable-value exp env))
 	((quoted? exp) (text-of-quotation exp))
@@ -76,9 +80,9 @@
 			 env))
 	((begin? exp)
 	 (eval-sequence (begin-actings exp) env))
-	((cond? exp) (eval (cond-if exp) env))
+	((cond? exp) (my-eval (cond-if exp) env))
 	((application? exp)
-	 (apply (eval (operator exp) env)
+	 (my-apply (eval (operator exp) env)
 		(list-of-values (operands exp) env)))
 	(else
 	 (error "Unknown expression type -- EVAL" exp))))
@@ -94,7 +98,7 @@
 ;;; 기본 프로시저가 오면 apply-primitive-procedure로 처리하고
 ;;; 합친 프로시저가 오면 그 몸이 되는 식을 차례대로 하나씩 처리한다.
 
-(define (apply procedure arguments)
+(define (my-apply procedure arguments)
   (cond ((primitive-procedure? procedure)
 	 (apply-primitive-procedure procedure arguments))
 	((compound-procedure? procedure)
@@ -118,7 +122,7 @@
 (define (list-of-values exps env)
   (if (no-operands? exps)
       '()
-      (cons (eval (first-operand exps) env)
+      (cons (my-eval (first-operand exps) env)
 	    (list-of-values (rest-operands exps) env))))
 
 
@@ -129,8 +133,8 @@
 
 (define (eval-if exp env)
   (if (ture? (eval (if-predicate exp) env))
-      (eval (if-consequent exp) env)
-      (eval (if-alternative exp) env)))
+      (my-eval (if-consequent exp) env)
+      (my-eval (if-alternative exp) env)))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -140,8 +144,8 @@
 ;;; eval이 begin으로 묶은 잇단식을 처리해야 할 때 eval-sequence를 쓴다.
 
 (define (eval-sequence exps env)
-  (cond ((last-exp? exps) (eval (first-exp exps) env))
-	(else (eval (first-exp exps) env)
+  (cond ((last-exp? exps) (my-eval (first-exp exps) env))
+	(else (my-eval (first-exp exps) env)
 	      (eval-sequence (rest-exps exps) env))))
 
 
@@ -151,14 +155,14 @@
 
 (define (eval-assignment exp env)
   (set-variable-value! (assignment-variable exp)
-		       (eval (assignment-value exp) env)
+		       (my-eval (assignment-value exp) env)
 		       env)
   'ok)
 
 
 (define (eval-definition exp env)
   (define-variable! (definition-variable exp)
-    (eval (definition-variable exp) env)
+    (my-eval (definition-variable exp) env)
     env)
   'ok)
 	  
@@ -182,7 +186,7 @@
 (define (list-of-values exps env)
   (if (no-operands? exps)
       '()
-      (cons (eval (first-operand exps) env)
+      (cons (my-eval (first-operand exps) env)
 	    (list-of-values (rest-operands exps) env))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -205,19 +209,633 @@
   (cons (f1) a))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; 1) 왼쪽에서 오른쪽으로
-(define (list-of-values exps env)
-  (if (no-operands? exps)
-      '()
-      (let ((left (eval (first-operand exps) env))
-	    (rest (list-of-values (rest-operands exps) env)))
-	(cons left rest))))
+;; ;; 1) 왼쪽에서 오른쪽으로
+;; (define (list-of-values exps env)
+;;   (if (no-operands? exps)
+;;       '()
+;;       (let ((left (my-eval (first-operand exps) env))
+;; 	    (rest (list-of-values (rest-operands exps) env)))
+;; 	(cons left rest))))
 
-;; 2) 오른쪽에서 왼쪽으로
-(define (list-of-values exps env)
-  (if (no-operands? exps)
-      '()
-      (let ((rest (list-of-values (rest-operands exps) env))
-	    (left (eval (first-operand exps) env)))
-	(cons left rest))))
+;; ;; 2) 오른쪽에서 왼쪽으로
+;; (define (list-of-values exps env)
+;;   (if (no-operands? exps)
+;;       '()
+;;       (let ((rest (list-of-values (rest-operands exps) env))
+;; 	    (left (my-eval (first-operand exps) env)))
+;; 	(cons left rest))))
+
+
+
+;;;==========================================
+;;; 4.1.2 식을 나타내는 방법
+;;; p481
+
+;; * 곧바로 값을 구할 수 있는 것은 수와 글줄(문자열-string) 뿐 이다.
+(define (self-evaluating? exp)
+  (cond ((number? exp) #t) ;true)
+	((string? exp) #t) ;true)
+	(else #f))) ;false)))
+
+;; * 변수는 글자로 나타낸다.
+(define (variable? exp) (symbol? exp))
+
+;; * 따온 식은 (quote <text-of-quotation>) 꼴로 나타낸다.
+(define (quoted? exp)
+  (tagged-list? exp 'quote))
+
+(define (text-of-quotation exp) (cadr exp))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      #f))
+
+;; * 덮어쓰기는 (set! <var> <value>)꼴로 나타낸다.
+(define (assignment? exp)
+  (tagged-list? exp 'set!))
+
+(define (assignment-variable exp) (cadr exp))
+
+(define (assignment-value exp) (caddr exp))
+
+;; * 정의는 다음 두 꼬러 가운데 하나다.
+;; - (define <var> <value>)
+;; - (define (<var> <parameter1> ... <parameter_n>) 
+;;     <body>)
+;;   위는 아래 식의 달콤한 문법이다.
+;;   (define <var>
+;;      (lambda (<parameter1> ... <parameter_n>)
+;;        <body>))
+(define (definition? exp)
+  (tagged-list? exp 'define))
+
+(define (definition-variable exp)
+  (if (symbol? (cadr exp))
+      (cadr exp)           ; 1) (define a 1)
+      (caadr exp)))        ; 2) (define (f x) ... )
+
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+      (caddr exp)
+      (make-lambda (cdadr exp)  ; 매개변수 이름
+		   (cddr exp))))
+
+;; * lambda 식은 lambda 글자로 시작하는 리스트다.
+(define (lambda? exp) (tagged-list? exp 'lambda))
+
+(define (lambda-parameters exp) (cadr exp))
+
+(define (lambda-body exp) (cddr exp))
+
+(define (make-lambda parameters body)
+  (cons 'lambda (cons parameters body)))
+
+;; * 조건식은 if로 시작하여 술어, 결과 식, 다른 결과 식으로 구성된다.
+;;   다른 결과 식이 없으면 그 값은 언제나 false
+(define (if? exp) (tagged-list? exp 'if))
+
+(define (if-predicate exp) (cadr exp))
+
+(define (if-consequent exp) (caddr exp))
+
+(define (if-alternative exp)
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      '#f)) ;'false))
+
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
+
+;; * begin은 잇단식을 하나로 묶어낸다.
+;; - 첫 식과 나머지 모든 식을 골라내는 고르개
+(define (begin? exp) (tagged-list? exp 'begin))
+
+(define (begin-actions exp) (cdr exp))
+
+(define (last-exp? seq) (null? (cdr seq)))
+
+(define (first-exp seq) (car seq))
+
+(define (rest-exps seq) (cdr seq))
+
+;; sequence->exp 짜맞추개
+(define (sequence->exp seq)
+  (cond ((null? seq) seq)
+	((last-exp? seq) (first-exp seq))
+	(else (make-begin seq))))
+
+(define (make-begin seq) (cons 'begin seq))
+
+;; * 프로시적 적용은 또 다른 합친 식이다.
+;;   식의 car는 연산자, cdr는 피연산자
+(define (application? exp) (pair? exp))
+;; exp :=
+;; ( (lambda (<parameters...>) <body>)
+;;   (<arguments ...>) )
+
+
+(define (operator exp) (car exp))
+
+(define (operands exp) (cdr exp))
+
+(define (no-operands? ops) (null? ops))
+
+(define (first-operand ops) (car ops))
+
+(define (rest-operands ops) (cdr ops))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; 이끌어낸 식 (derived expression)
+;;; p485
+
+;; 따로 프로시저를 만들어 처리하지 않아도 다른 특별한 형태의 힘을 빌려 처리할 수 있는 것
+;; : 계산 방법을 따로 만들어 주어야 할 특별한 형태가 줄어들기 때문에,
+;;   실행기가 단순해진다.
+;; (cond ((> x 0) x)
+;;       ((= x 0) (display 'zero) 0)
+;;       (else (- x)))
+;; ;;=>
+;; (if (> x 0)
+;;     x
+;;     (if (= x 0)
+;; 	(begin (display 'zero)
+;; 	       0)
+;; 	(- x)))
+
+;; cond 에 대한 ..
+
+(define (cond? exp) (tagged-list? exp 'cond))
+
+(define (cond-clauses exp) (cdr exp))
+
+(define (cond-else-clause? clause)
+  (eq? (cond-predicate clause) 'else))
+
+(define (cond-predicate clause) (car clause))
+
+(define (cond-actions clause) (cdr clause))
+
+(define (cond->if exp)
+  (expand-clauses (cond-clauses exp)))
+
+(define (expand-clauses clauses)
+  (if (null? clauses)
+      '#f ; 'false    ; else 절이 없을 경우
+      (let ((first (car clauses))
+	    (rest (cdr clauses)))
+	(if (cond-else-clause? first)
+	    (if (null? rest)
+		(sequence->exp (cond-actions first))
+		(error "ELSE clause isn't last -- COND->IF"
+		       clauses))
+	    (make-if (cond-predicate first)
+		     (sequence->exp (cond-actions first))
+		     (expand-clauses rest))))))
+
+
+
+;;;--------------------------< ex 4.2 >--------------------------
+;;; p487
+
+;; eval 프로시저의 cond절에서 
+;;  덮어쓰기 연산, 정의보다
+;;  프로시저 적용이 먼저오게 나오게 하면?
+
+;; a) 잘못된 점은?
+;;  exp : (define x 3)을 처리할 때
+;; 1) (application? exp) 에서 pair? -> ture가 되어서 apply로 넘어감
+;; 2) -> operator : define
+;;    (eval 'define env) ...
+;; 
+
+;; b) 프로시저 적용식이 언제나 call로 시작되게 하면?
+;; (factorial 3) -> (call factorial 3)
+;; (+ 1 2) -> (call + 1 2)
+
+;; 그러면 기존에 정의한 프로시저를 조금 바꿔서 적용 가능할 듯...
+
+
+
+;;;--------------------------< ex 4.3 >--------------------------
+;;; p488
+
+;; 기존 eval 테스트
+(my-eval 1 '())
+
+;; (my-eval '(define a 1) '())
+
+
+;;;-----------------------------------------------
+;;; put, get 구현
+;; (define SFT (make-hash))
+
+;; (define (put op types behavior)
+;;   (hash-set! SFT (cons op types) behavior))
+;; ;;  (hash-set! GOP (cons 'key (cons op types)) behavior))
+
+;; (define (get op types)
+;;   (hash-ref SFT (cons op types)))
+;; ;;  (hash-ref GOP (cons 'key (cons op types))))
+;; ;;;---
+
+
+;; ;;; eval 정의
+;; (define (my-eval exp env)
+;;   (cond ((self-evaluating? exp) exp)
+;; 	((variable? exp) (lookup-variable-value exp env))
+;; 	((quoted? exp) (text-of-quotation exp))
+;; 	((assignment? exp) (eval-assignment exp env))
+;; 	((definition? exp) (eval-definition exp env))
+;; 	((if? exp) (eval-if exp env))
+;; 	((lambda? exp)
+;; 	 (make-procedure (lambda-parameters exp)
+;; 			 (lambda-body exp)
+;; 			 env))
+;; 	((begin? exp)
+;; 	 (eval-sequence (begin-actings exp) env))
+;; 	((cond? exp) (my-eval (cond-if exp) env))
+;; 	((application? exp)
+;; 	 (my-apply (eval (operator exp) env)
+;; 		(list-of-values (operands exp) env)))
+;; 	(else
+;; 	 (error "Unknown expression type -- EVAL" exp))))
+;; =>
+
+;; ;;; eval 정의
+;; (define (my-eval exp env)
+;;   (cond ((self-evaluating? exp) exp)
+;; 	((variable? exp) (lookup-variable-value exp env))
+
+;; 	((application? exp)
+;; 	 (my-apply (eval (operator exp) env)
+;; 		(list-of-values (operands exp) env)))
+;; 	(else
+;; 	 (error "Unknown expression type -- EVAL" exp))))
+
+;; (define (install-my-eval1-package)
+;;   ;; 갇힌 프로시저
+
+;;   ;;----------------------------------------------
+;;   ;; * 따온 식- 'quote
+;;   (define (quoted? exp)
+;;     (tagged-list? exp 'quote))
+
+;;   (define (text-of-quotation exp) (cadr exp))
+
+;;   (define (tagged-list? exp tag)
+;;     (if (pair? exp)
+;; 	(eq? (car exp) tag)
+;; 	#f))
+
+;;   ;;----------------------------------------------
+;;   ;; * 덮어쓰기 - 'set!
+;;   (define (assignment? exp)
+;;     (tagged-list? exp 'set!))
+
+;;   (define (assignment-variable exp) (cadr exp))
+
+;;   (define (assignment-value exp) (caddr exp))
+
+;;   ;; * 정의 - 'define
+;;   (define (definition? exp)
+;;     (tagged-list? exp 'define))
+
+;;   (define (definition-variable exp)
+;;     (if (symbol? (cadr exp))
+;; 	(cadr exp)           ; 1) (define a 1)
+;; 	(caadr exp)))        ; 2) (define (f x) ... )
+
+;;   (define (definition-value exp)
+;;     (if (symbol? (cadr exp))
+;; 	(caddr exp)
+;; 	(make-lambda (cdadr exp)  ; 매개변수 이름
+;; 		     (cddr exp))))
+
+;;   ;;----------------------------------------------
+;;   ;; * 'lambda 
+;;   (define (lambda? exp) (tagged-list? exp 'lambda))
+
+;;   (define (lambda-parameters exp) (cadr exp))
+
+;;   (define (lambda-body exp) (cddr exp))
+
+;;   (define (make-lambda parameters body)
+;;     (cons 'lambda (cons parameters body)))
+
+;;   ;;----------------------------------------------
+;;   ;; * 'if
+;;   (define (if? exp) (tagged-list? exp 'if))
+
+;;   (define (if-predicate exp) (cadr exp))
+
+;;   (define (if-consequent exp) (caddr exp))
+
+;;   (define (if-alternative exp)
+;;     (if (not (null? (cdddr exp)))
+;; 	(cadddr exp)
+;; 	'#f)) ;'false))
+
+;;   (define (make-if predicate consequent alternative)
+;;     (list 'if predicate consequent alternative))
+
+;;   ;;----------------------------------------------
+;;   ;; * 'begin
+;;   (define (begin? exp) (tagged-list? exp 'begin))
+
+;;   (define (begin-actions exp) (cdr exp))
+
+;;   (define (last-exp? seq) (null? (cdr seq)))
+
+;;   (define (first-exp seq) (car seq))
+
+;;   (define (rest-exps seq) (cdr seq))
+
+;;   ;; sequence->exp 짜맞추개
+;;   (define (sequence->exp seq)
+;;     (cond ((null? seq) seq)
+;; 	  ((last-exp? seq) (first-exp seq))
+;; 	  (else (make-begin seq))))
+
+;;   (define (make-begin seq) (cons 'begin seq))
+;;   ;;----------------------------------------------
+
+;;   ;; 이 꾸러미의 인터페이스
+;;   (define (tag x) (attach-tag 'rectangular x))
+;;   (put 'quote 'my-eval1 text-of-quotation)
+;;   (put 'set! 'my-eval1 eval-assignment)
+;;   (put 'define 'my-eval1 eval-definition)
+;;   (put 'if 'my-eval1 eval-if)
+;;   (put 'lambda 'my-eval1 make-procedure)
+;;   (put 'begin 'my-eval1 eval-sequence)
+;;   (put 'cond 'my-eval1 my-eval)  ;; <<--
+
+;;   'done)
+
+
+
+;;;--------------------------< ex 4.4 >--------------------------
+;;; p488
+
+;; and, or 정의하기
+
+;; a) 특별한 형태로 정의
+;; (and a b c)
+(define (and? exp) (tagged-list? exp 'and))
+
+(define (rest-predicates exp) (cdr exp))
+
+(define (eval-and exp env)
+  (define (eval-and-inner predicates env2)
+    (let ((a-predicates (car predicates)))
+      (let ((a-res (my-eval a-preicates env2)))
+	(if a-res
+	    (if (null? (cdr predicates))
+		a-res
+		(eval-and-inner (resdt-predicates predicates) env2))
+	    #f))))
+  (eval-and-inner (rest-predicates exp) env))
+
+
+(define (eval-or exp env)
+  (define (eval-or-inner predicates env2)
+    (let ((a-predicates (car predicates)))
+      (let ((a-res (my-eval a-preicates env2)))
+	(if a-res
+	    #t
+	    (if (null? (cdr predicates))
+		a-res
+		(eval-or-inner (resdt-predicates predicates) env2))))))
+  (eval-or-inner (rest-predicates exp) env))
+
+;; b) 이끌어낸 식으로 정의
+;; cond를 expand할 때는 표현의 문법 구조만 보고 그대로 확장한다
+;; and/or를 확장할 때는 표현 각각의 값을 평가한 다음에 결과에 따라서 확장한다.
+
+
+;;;--------------------------< ex 4.5 >--------------------------
+;;; p489
+
+(cond ((assoc 'b '((a 1) (b 2))) => cadr)
+      (else false))
+
+;;;--------------------------< ex 4.6 >--------------------------
+;;; p489
+
+
+
+;;;--------------------------< ex 4.7 >--------------------------
+;;; p489
+
+
+
+;;;--------------------------< ex 4.8 >--------------------------
+;;; p490
+
+
+
+;;;--------------------------< ex 4.9 >--------------------------
+;;; p491
+
+
+
+;;;--------------------------< ex 4.10 >--------------------------
+;;; p491
+
+
+
+
+
+;;;==========================================
+;;; 4.1.3 언어 실행기에서 쓰는 데이터 구조
+;;; p491
+
+;;; 겉으로 드러나는 식의 문법 뿐 아니라
+;;; 실행기 프로그램이 돌아가는 데 꼭 필요한 데이터 구조를 정의해야 한다.
+;;; ( 프로시저, 화녁ㅇ의 표현 방법, 참이나 거짓 값의 표현 방법 )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; 술어 검사하기
+;;; false 아닌 값이 오면 모두 참이라고 본다.
+(define (true? x)
+  (not (eq? x #f)))
+
+(define (false? x)
+  (eq? x #f))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; 프로시저 표현
+;;; 언어의 기본 원소를 처리할 때, 다음과 같은 프로시저를 쓸 수 있다고 가정한다.
+
+;; * (apply-primitive-procedure <proc> <args>
+;; : <proc>에 <arg>리스트에 들어 있는 인자 값을 건네주고 그 값을 구할 때 쓴다.
+
+;; * (primitive-procedure? <proc>
+;; : <proc>이 기본 프로시저인지 아닌지 물어보는데 쓴다.
+
+;; 인자, 프로시저 몸, 환경을 받아 새로 합친 프로시저를 만드는 짜맞추개
+(define (make-procedure parameters body env)
+  (list 'procedure parameters body env))
+
+(define (compound-procedure? p)
+  (tagged-list? p 'procedure))
+
+(define (procedure-parameters p) (cadr p))
+
+(define (procedure-body p) (caddr p))
+
+(define (procedure-environment p) (cadddr p))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; 환경 연산
+;;; 환경 : 여러 변수 일람표를 한 줄로 이어놓은 것
+;;; 변수 일람표 : 변수와 값을 한 쌍으로 묶어둔 표
+
+;; * (look-variable-value <var> <env>
+;; : <env>를 뒤져서 <var>와 짝을 이루는 값을 찾아서 내놓는다.
+
+;; * (extend-environment <variables> <values> <base-env>)
+;; : 새 환경을 내놓는다.
+
+;; * (define-variable! <var> <value> <env>)
+;; : <env>환경의 첫번째 일람표에 <var> <value> 정의 집어넣기
+
+;; * (set-variable-value! <var> <value> <env>)
+;; : 값 고치기
+
+;;; 환경을 변수 일람표들의 리스트로 표현
+;; (cons '((c) (3)) (cons '((b) (2)) '((a) (1)))))
+(define (enclosing-environment env) (cdr env))
+
+(define (first-frame env) (car env))
+
+(define the-empty-environment '())
+
+;;; 변수 일람표는 두 리스트의 쌍으로 나타낸다.
+;;; ((var1 var2 ...) val1 val2 ...)
+(define (make-frame variables values)
+  (cons variables values))
+
+(define (frame-variables frame) (car frame))
+
+(define (frame-values frame) (cdr frame))
+
+(define (add-binding-to-frame! var val frame)
+  (set-car! frame (cons var (car frame)))
+  (set-cdr! frame (cons val (cdr frame))))
+
+;;------
+(make-frame 'a 1)  ; '(a . 1)
+
+(define frame1 (make-frame '(a b c) '(1 2 3)))
+frame1
+ ; '((a b c) 1 2 3)
+
+(add-binding-to-frame! 'd 4 frame1)
+frame1
+;((d a b c) 4 1 2 3)
+;;------
+
+
+;; 환경 넓히기
+;; - 변수 리스트와 값 리스트로 새 변수 일람표를 만든 다음에 이를 환경에 이어 붙임
+(define (extend-environment vars vals base-env)
+  (if (= (length vars) (length vals))
+      (cons (make-frame vars vals) base-env)
+      (if (< (length vars) (length vals))
+	  (error "Too many arguments supplied" vars vals)
+	  (error "Too few arguments supplied" vars vals))))
+
+;;------
+(define env1 (extend-environment '(a b) '(1 2) the-empty-environment))
+env1
+;; '(((a b) 1 2))
+
+(define env2 (extend-environment '(c d) '(3 4) env1))
+env2
+;; '(((c d) 3 4) ((a b) 1 2))
+
+(enclosing-environment env2)
+;; '(((a b) 1 2))
+;;------
+
+;; 지정된 환경에서 변수를 찾을 때
+;; - 첫 번째 일람표의 변수 리스트...
+;;   없으면 그 일람표를 둘러싼 환경
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+	     (env-loop (enclosing-environment env)))
+	    ((eq? var (car vars))
+	     (car vals))
+	    (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+	(error "Unbound variable" var)
+	(let ((frame (first-frame env)))
+	  (scan (frame-variables frame)
+		(frame-values frame)))))
+  (env-loop env))
+
+
+
+;; 정해진 환경에서 어떤 변수 값을 고쳐쓸 때
+(define (set-variable-value! var val env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+	     (env-loop (enclosing-environment env)))
+	    ((eq? var (car vars))
+	     (set-car! vals val))
+	    (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+	(error "Unbound variable -- SET!" var)
+	(let ((frame (first-frame env)))
+	  (scan (frame-variables frame)
+		(frame-values frame)))))
+  (env-loop env))
+
+;;;-----------
+env2
+(set-variable-value! 'a 5 env2)
+env2
+(set-variable-value! 'a 1 env2)
+env2
+;;;-----------
+
+
+;; 변수를 정의할 때
+(define (define-variable! var val env)
+  (let ((frame (first-frame env)))
+    (define (scan vars vals)
+      (cond ((null? vars)
+	     (add-binding-to-frame! var val frame))
+	    ((eq? var (car vars))
+	     (set-car! vals val))
+	    (else (scan (cdr vars) (cdr vals)))))
+    (scan (frame-variables frame)
+	  (frame-values frame))))
+
+env2 ;(((c d) 3 4) ((a b) 1 2))
+(define-variable! 'e 5 env2)
+env2 ;(((e c d) 5 3 4) ((a b) 1 2))
+
+;;;--------------------------< ex 4.11 >--------------------------
+;;; p496
+
+
+
+;;;--------------------------< ex 4.12 >--------------------------
+;;; p497
+
+
+
+;;;--------------------------< ex 4.13 >--------------------------
+;;; p497
+
+
+
 
