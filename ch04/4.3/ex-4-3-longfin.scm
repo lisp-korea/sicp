861c8774b4cabe82fbed87578bdb1a7c15c859d0
longfin <longfinfunnel@gmail.com>
2011-07-19 14:14:06 +0900

    -solve ex 4.37

--- ch04/4.3/ex-4-3-longfin.scm
+++ ch04/4.3/ex-4-3-longfin.scm
@@ -139,3 +139,61 @@
 ;; 2^2 + 2^2 vs 2^2
 ;; 1^2 + 1^2 vs 3^2
 ;; ...
+
+
+;; ex 4.37
+(define (a-pythagorean-triple-between low high)
+  (let ((i (an-integer-between low high))
+		(hsq (* high high)))
+	(let ((j (an-integer-between i high)))
+	  (let ((ksq (+ (* i i) (* j j))))
+		(require (>= hsq ksq))
+		(let ((k (sqrt ksq)))
+		  (require (integer? k))
+		  (list i j k))))))
+
+;; hsq = high * high
+;; ksq = i^2 + j^2
+;; filter hsq >= ksq
+;; filter sqrt ksq(=k) is integer
+
+;; (a-pythagorean-triple-between 1 10)
+
+;; high = 10
+;; low = 1
+
+;; [amb-1]
+;; i = 1
+
+;; hsq = 100
+
+;; [amb-2]
+;; j = 1
+;; ksq = 2
+
+;; require 100 >= 2
+;; k = sqrt 2
+
+;; require k is integer => false : to amb-2
+
+;; [amb-2]
+;; j == i :to amb-1
+
+;; [amb-1]
+;; i = 2
+
+;; hsq = 100
+
+;; [amb-2]
+;; j = 1
+;; ksq = 5
+
+;; require 100 >= 5
+;; k = sqrt 5
+
+;; require k isn't integer => false : to amb-2
+
+;; ...
+
+
+;; it uses only 2 continuation.(i, j) so time complexity is O(n^2)[4.35's solution is O(n^3)]
