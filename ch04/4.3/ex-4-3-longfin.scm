c49a0e48b293db7d990338508f08559f2f09c2f3
longfin <longfinfunnel@gmail.com>
2011-07-19 14:37:14 +0900

    -Solve ex 4.38

--- ch04/4.3/ex-4-3-longfin.scm
+++ ch04/4.3/ex-4-3-longfin.scm
@@ -197,3 +197,80 @@
 
 
 ;; it uses only 2 continuation.(i, j) so time complexity is O(n^2)[4.35's solution is O(n^3)]
+
+;; 4.3.2 Examples of Nondeterministic Programs
+
+;; Logic Puzzles
+
+(define (distinct? list)
+  (define (find el lst)
+	 (cond
+	  ((null? lst) #f)
+	  ((= el (car lst)) #t)
+	  (else (find el (cdr lst)))))		  
+  (cond ((null? list) #t)
+		((= 1 (length list)) #t)
+		(else
+		 (if (find (car list) (cdr list))
+			 #f
+			 (distinct? (cdr list))))))
+(define (multiple-dwelling)
+  (let ((baker (amb 1 2 3 4 5))
+		(cooper (amb 1 2 3 4 5))
+		(fletcher (amb 1 2 3 4 5))
+		(miller (amb 1 2 3 4 5))
+		(smith (amb 1 2 3 4 5)))
+	(require
+	 (distinct? (list baker cooper fletcher miller smith)))
+	(require (not (= baker 5)))
+	(require (not (= cooper 1)))
+	(require (not (= fletcher 5)))
+	(require (not (= fletcher 1)))
+	(require (> miller cooper))
+	(require (not (= (abs (- smith fletcher)) 1)))
+	(require (not (= (abs (- fletcher cooper)) 1)))
+	(list (list 'baker baker)
+		  (list 'cooper cooper)
+		  (list 'fletcher fletcher)
+		  (list 'miller miller)
+		  (list 'smith smith))))
+
+
+;; ex 4.38
+
+		  
+(define (multiple-dwelling)
+  (let ((baker (amb 1 2 3 4 5))
+		(cooper (amb 1 2 3 4 5))
+		(fletcher (amb 1 2 3 4 5))
+		(miller (amb 1 2 3 4 5))
+		(smith (amb 1 2 3 4 5)))
+	(require
+	 (distinct? (list baker cooper fletcher miller smith)))
+	(require (not (= baker 5)))
+	(require (not (= cooper 1)))
+	(require (not (= fletcher 5)))
+	(require (not (= fletcher 1)))
+	(require (> miller cooper))
+;;	(require (not (= (abs (- smith fletcher)) 1)))
+	(require (not (= (abs (- fletcher cooper)) 1)))
+	(list (list 'baker baker)
+		  (list 'cooper cooper)
+		  (list 'fletcher fletcher)
+		  (list 'miller miller)
+		  (list 'smith smith))))
+
+;; > (multiple-dwelling)
+;; ((baker 1) (cooper 2) (fletcher 4) (miller 3) (smith 5))
+;; > (try-again)
+;; ((baker 1) (cooper 2) (fletcher 4) (miller 5) (smith 3))
+;; > (try-again)
+;; ((baker 1) (cooper 4) (fletcher 2) (miller 5) (smith 3))
+;; > (try-again)
+;; ((baker 3) (cooper 2) (fletcher 4) (miller 5) (smith 1))
+;; > (try-again)
+;; ((baker 3) (cooper 4) (fletcher 2) (miller 5) (smith 1))
+;; > (try-again)
+;; ((baker 1) (cooper 2) (fletcher 4) (miller 3) (smith 5))
+;; > (try-again)
+;; ((baker 1) (cooper 2) (fletcher 4) (miller 5) (smith 3))
