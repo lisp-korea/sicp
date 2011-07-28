b38c4fda40bcc695d8eb701048e9f5b4315604c4
longfin <longfinfunnel@gmail.com>
2011-07-28 21:39:46 +0900

    -fix ex 4.48 -Solve ex 4.50

--- ch04/4.3/ex-4-3-longfin.scm
+++ ch04/4.3/ex-4-3-longfin.scm
@@ -699,9 +699,14 @@ try-again
 		(adjectives-noun-rule (lambda (rules)
 								(list 'noun-phrase
 									  (parse-word adjectives)
-									  (inner rules)))))
+									  (inner rules))))
+		(noun-prep-rule (lambda (rules)
+						  (list 'noun-phrase
+								(parse-word nouns)
+								(parse-prepositional-phrase)))))
 	(inner (list article-noun-rule
-				 adjectives-noun-rule))))
+				 adjectives-noun-rule
+				 noun-prep-rule))))
 
 (define (parse-verb-phrase)
   (define (inner rules)
@@ -735,3 +740,387 @@ try-again
 	 (car lst)
 	 (generate (cdr lst))))
   (list (car word-list) (generate (cdr word-list))))
+
+
+;; 4.3.3 Implementing the amb Evaluator
+
+;; Execution procedures and continuations
+
+;; Failure continuations are constructed by
+;; - amb expressions -- to provide a mechanism to make alternative choices if the current choice made by the amb expression leads to a dead end;
+;; - the top-level driver -- to provide a mechanism to report failure when the choices are exhausted;
+;; - assignments -- to intercept failures and undo assignments during backtracking.
+
+;; Failures are initiated only when a dead end is encountered. This occurs
+;; - if the user program executes (amb);
+;; - if the user types try-again at the top-level driver.
+
+;; Failure continuations are also called during processing of a failure:
+;; - When the failure continuation created by an assignment finishes undoing a side effect, it calls the failure continuation it intercepted, in order to propagate the failure back to the choice point that led to this assignment or to the top level.
+;; - When the failure continuation for an amb runs out of choices, it calls the failure continuation that was originally given to the amb, in order to propagate the failure back to the previous choice point or to the top level.
+
+
+;; analyze from 4.1.6, with clause from 4.3.3 added
+;; and also support for Let
+(define (analyze exp)
+  (cond ((self-evaluating? exp) 
+         (analyze-self-evaluating exp))
+        ((quoted? exp) (analyze-quoted exp))
+        ((variable? exp) (analyze-variable exp))
+        ((assignment? exp) (analyze-assignment exp))
+        ((definition? exp) (analyze-definition exp))
+        ((if? exp) (analyze-if exp))
+        ((lambda? exp) (analyze-lambda exp))
+        ((begin? exp) (analyze-sequence (begin-actions exp)))
+        ((cond? exp) (analyze (cond->if exp)))
+        ((let? exp) (analyze (let->combination exp))) ;**
+        ((amb? exp) (analyze-amb exp))                ;**
+        ((application? exp) (analyze-application exp))
+        (else
+         (error "Unknown expression type -- ANALYZE" exp))))
+
+;;; Support for Let (as noted in footnote 56, p.428)
+
+(define (let? exp) (tagged-list? exp 'let))
+(define (let-bindings exp) (cadr exp))
+(define (let-body exp) (cddr exp))
+
+(define (let-var binding) (car binding))
+(define (let-val binding) (cadr binding))
+
+(define (make-combination operator operands) (cons operator operands))
+
+(define (let->combination exp)
+  ;;make-combination defined in earlier exercise
+  (let ((bindings (let-bindings exp)))
+    (make-combination (make-lambda (map let-var bindings)
+                                   (let-body exp))
+                      (map let-val bindings))))
+                     
+;; Structure of the evaluator
+
+(define (amb? exp) (tagged-list? exp 'amb))
+(define (amb-choices exp) (cdr exp))
+
+(define (ambeval exp env succeed fail)
+  ((analyze exp) env succeed fail))
+
+
+;; (ambeval <exp> the-global-environment
+;;          (lambda (value fail) value)
+;;          (lambda () 'failed))
+
+
+
+;; Simple expressions
+
+;; before
+;; (define (analyze-self-evaluating exp)
+;;   (lambda (env) exp))
+
+(define (analyze-self-evaluating exp)
+  (lambda (env succeed fail)
+    (succeed exp fail)))
+
+;; before
+;; (define (analyze-quoted exp)
+;;   (let ((qval (text-of-quotation exp)))
+;; 	(lambda (env) qval)))
+
+(define (analyze-quoted exp)
+  (let ((qval (text-of-quotation exp)))
+    (lambda (env succeed fail)
+      (succeed qval fail))))
+
+;; before
+;; (define (analyze-variable exp)
+;;   (lambda (env) (lookup-variable-value exp env)))
+
+(define (analyze-variable exp)
+  (lambda (env succeed fail)
+    (succeed (lookup-variable-value exp env)
+             fail)))
+
+;; before
+;; (define (analyze-lambda exp)
+;;   (let ((vars (lambda-parameters exp))
+;; 		(bproc (analyze-sequence (lambda-body exp))))
+;; 	(lambda (env) (make-procedure-with-analyze vars bproc env))))
+
+(define (analyze-lambda exp)
+  (let ((vars (lambda-parameters exp))
+        (bproc (analyze-sequence (lambda-body exp))))
+    (lambda (env succeed fail)
+      (succeed (make-procedure vars bproc env)
+			   fail))))
+
+
+;; Conditionals and Sequences
+;; before
+;; (define (analyze-if exp)
+;;   (let ((pproc (analyze (if-predicate exp)))
+;; 		(cporc (analyze (if-consequent exp)))
+;; 		(aproc (analyze (if-alternative exp))))
+;; 	(lambda (env)
+;; 	  (if (true? (pproc env))
+;; 		  (cporc env)
+;; 		  (aproc env)))))
+
+(define (analyze-if exp)
+  (let ((pproc (analyze (if-predicate exp)))
+        (cproc (analyze (if-consequent exp)))
+        (aproc (analyze (if-alternative exp))))
+	(lambda (env succeed fail)
+      (pproc env
+             ;; success continuation for evaluating the predicate
+             ;; to obtain pred-value
+             (lambda (pred-value fail2)
+               (if (true? pred-value)
+                   (cproc env succeed fail2)
+                   (aproc env succeed fail2)))
+             ;; failure continuation for evaluating the predicate
+			 fail))))
+
+;; before
+;; (define (analyze-sequence exps)
+;;   (define (sequentially proc1 proc2)
+;; 	(lambda (env) (proc1 env) (proc2 env)))
+;;   (define (loop first-proc rest-procs)
+;; 	(if (null? rest-procs)
+;; 		first-proc
+;; 		(loop (sequentially first-proc (car rest-procs))
+;; 			  (cdr rest-procs))))
+;;   (let ((procs (map analyze exps)))
+;; 	(if (null? procs)
+;; 		(error "Empty sequence -- ANALYZE"))
+;; 	(loop (car procs) (cdr procs))))
+
+(define (analyze-sequence exps)
+  (define (sequentially a b)
+    (lambda (env succeed fail)
+      (a env
+         ;; success continuation for calling a
+         (lambda (a-value fail2)
+           (b env succeed fail2))
+         ;; failure continuation for calling a
+         fail)))
+  (define (loop first-proc rest-procs)
+    (if (null? rest-procs)
+        first-proc
+        (loop (sequentially first-proc (car rest-procs))
+              (cdr rest-procs))))
+  (let ((procs (map analyze exps)))
+    (if (null? procs)
+        (error "Empty sequence -- ANALYZE"))
+    (loop (car procs) (cdr procs))))
+
+;; Definitions and assignments
+
+;; before
+;; (define (analyze-definition exp)
+;;   (let ((var (definition-variable exp))
+;; 		   (vproc (analyze (definition-value exp))))
+;; 	(lambda (env)
+;; 	  (define-variable! var (vproc env) env)
+;; 	  'ok)))
+
+(define (analyze-definition exp)
+  (let ((var (definition-variable exp))
+        (vproc (analyze (definition-value exp))))
+    (lambda (env succeed fail)
+	  (vproc env
+			 (lambda (val fail2)
+			   (define-variable! var val env)
+			   (succeed 'ok fail2))
+			 fail))))
+
+
+;; before
+;; (define (analyze-assignment exp)
+;;   (let ((var (assignment-variable exp))
+;; 		(vproc (analyze (assignment-value exp))))
+;; 	(lambda (env)
+;; 	  (set-variable-value! var (vproc env) env)
+;; 	  'ok)))
+
+(define (analyze-assignment exp)
+  (let ((var (assignment-variable exp))
+        (vproc (analyze (assignment-value exp))))
+    (lambda (env succeed fail)
+	  (vproc env
+			 (lambda (val fail2) ; *1*
+			   (let ((old-value
+					  (lookup-variable-value var env)))
+				 (set-variable-value! var val env)
+				 (succeed 'ok
+						  (lambda () ; *2*
+							(set-variable-value! var
+												 old-value
+												 env)
+							(fail2)))))
+			   fail))))
+
+;; Procedure applications
+
+;; before
+;; (define (analyze-application exp)
+;;   (let ((fproc (analyze (operator exp)))
+;; 		(aprocs (map analyze (operands exp))))
+;; 	(lambda (env)
+;; 	  (execute-application (fproc env)
+;; 						   (map (lambda (aproc) (aproc env))
+;; 								aprocs)))))
+
+(define (analyze-application exp)
+  (let ((fproc (analyze (operator exp)))
+        (aprocs (map analyze (operands exp))))
+    (lambda (env succeed fail)
+      (fproc env
+             (lambda (proc fail2)
+               (get-args aprocs
+                         env
+                         (lambda (args fail3)
+                           (execute-application
+                            proc args succeed fail3))
+                         fail2))
+			 fail))))
+
+(define (get-args aprocs env succeed fail)
+  (if (null? aprocs)
+      (succeed '() fail)
+      ((car aprocs) env
+	   ;; success continuation for this aproc
+	   (lambda (arg fail2)
+		 (get-args (cdr aprocs)
+				   env
+				   ;; success continuation for recursive
+				   ;; call to get-args
+				   (lambda (args fail3)
+					 (succeed (cons arg args)
+							  fail3))
+				   fail2))
+	   fail)))
+
+
+;; before
+;; (define (execute-application proc args)
+;;   (cond ((primitive-procedure? proc)
+;; 		 (apply-primitive-procedure proc args))
+;; 		((compound-procedure? proc)
+;; 		 ((procedure-body proc)
+;; 		  (extend-environment (procedure-parameters proc)
+;; 							  args
+;; 							  (procedure-environment proc))))
+;; 		(else
+;; 		 (error
+;; 		  "Unknown procedure type -- EXECUTE-APPLICATION"
+;; 		  proc))))
+
+(define (execute-application proc args succeed fail)
+  (cond ((primitive-procedure? proc)
+         (succeed (apply-primitive-procedure proc args)
+                  fail))
+        ((compound-procedure? proc)
+         ((procedure-body proc)
+          (extend-environment (procedure-parameters proc)
+                              args
+							  (procedure-environment proc))
+		  succeed
+		  fail))
+		(else
+         (error
+          "Unknown procedure type -- EXECUTE-APPLICATION"
+          proc))))
+
+;; Evaluating amb expressions
+
+(define (analyze-amb exp)
+  (let ((cprocs (map analyze (amb-choices exp))))
+    (lambda (env succeed fail)
+      (define (try-next choices)
+        (if (null? choices)
+            (fail)
+            ((car choices) env
+                           succeed
+                           (lambda ()
+                             (try-next (cdr choices))))))
+      (try-next cprocs))))
+
+
+;; Driver loop
+
+(define input-prompt ";;; Amb-Eval input:")
+(define output-prompt ";;; Amb-Eval value:")
+
+
+(define (driver-loop)
+  (define (internal-loop try-again)
+    (prompt-for-input input-prompt)
+    (let ((input (read)))
+      (if (eq? input 'try-again)
+          (try-again)
+          (begin
+            (newline)
+            (display ";;; Starting a new problem ")
+            (ambeval input
+                     the-global-environment
+                     ;; ambeval success
+                     (lambda (val next-alternative)
+                       (announce-output output-prompt)
+                       (user-print val)
+                       (internal-loop next-alternative))
+                     ;; ambeval failure
+                     (lambda ()
+                       (announce-output
+                        ";;; There are no more values of")
+                       (user-print input)
+                       (driver-loop)))))))
+  (internal-loop
+   (lambda ()
+     (newline)
+     (display ";;; There is no current problem")
+     (driver-loop))))
+
+
+;; ex 4.50
+;; using mit-scheme...
+
+(define (ramb? exp) (tagged-list? exp 'ramb))
+(define (ramb-choices exp) (cdr exp))
+
+
+(define (analyze exp)
+  (cond ((self-evaluating? exp) 
+         (analyze-self-evaluating exp))
+        ((quoted? exp) (analyze-quoted exp))
+        ((variable? exp) (analyze-variable exp))
+        ((assignment? exp) (analyze-assignment exp))
+        ((definition? exp) (analyze-definition exp))
+        ((if? exp) (analyze-if exp))
+        ((lambda? exp) (analyze-lambda exp))
+        ((begin? exp) (analyze-sequence (begin-actions exp)))
+        ((cond? exp) (analyze (cond->if exp)))
+        ((let? exp) (analyze (let->combination exp))) ;**
+        ((amb? exp) (analyze-amb exp))                ;**
+		((ramb? exp) (analyze-ramb exp))
+        ((application? exp) (analyze-application exp))
+        (else
+         (error "Unknown expression type -- ANALYZE" exp))))
+
+
+(define (analyze-ramb exp)
+  (let ((cprocs (map analyze (amb-choices exp))))
+    (lambda (env succeed fail)
+      (define (try-next choices)
+        (if (null? choices)
+            (fail)
+			(let ((i (random (length choices))))
+			  (let ((choosen (list-ref choices i)))
+				(let ((rest (filter (lambda (c) (not (eq? c choosen))) choices)))
+				  (choosen env
+						   succeed
+						   (lambda ()
+							 (try-next rest))))))))
+	  (try-next cprocs))))
+
+
