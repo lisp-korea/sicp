abfc677df9496ea1e290dcf2c4c349cd497ea4ba
suil son <likerivers12@gmail.com>
2011-08-13 03:39:35 +0900

    문제풀이 코드 추가

--- ch04/4.3/ex-4-3-likerivers12.scm
+++ ch04/4.3/ex-4-3-likerivers12.scm
@@ -4,10 +4,12 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Ch 4 언어를 처리하는 기법
+;;; Ch 4.3. Scheme 바꿔보기 - 비결정적 계산
+;;; p538
 
 
 ;;;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
+;;; 매크로를 이용한 amb 구현
 
 (define-syntax amb
   (syntax-rules ()
@@ -47,34 +49,39 @@
 ;;;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
-;;; Ch 4.3. Scheme 바꿔보기 - 비결정적 계산
-;;; p538
-
-
 
 ;;;==========================================
 ;;; 4.3.1. amb와 찾기
 ;;; p541
 
+(load "./amb-eval-likerivers12.scm")
+(driver-loop)
+
 (define (require p)
   (if (not p) (amb)))
 
+
+;;------------------
+;; amb 실행기에서 실행
 (define (an-element-of items)
   (require (not (null? items)))
   (amb (car items) (an-element-of (cdr items))))
 
+(an-element-of '(1 2 3))
+try-again
+try-again
+
+
+
 (define (an-integer-starting-from n)
   (amb n (an-integer-starting-from (+ n 1))))
 
+(an-integer-starting-from 10)
+try-again
+try-again
 
-;; 체계적으로 갈림기을 찾는다.
+;; 체계적으로 갈림길을 찾는다.
 ;; 깊이 먼저 찾기 방식으로
-
-;;;;;;;;;;;;;;;;;;;;;;;;
-;;; 드라이버 루프
-;;; p544
-
-
 
 ;;;--------------------------< ex 4.35 >--------------------------
 ;;; 피타고라스 수
@@ -92,7 +99,8 @@
   (amb low (an-integer-between (+ 1 low) high)))
 
 (a-pythagorean-triple-between 1 10)
-
+try-again
+try-again
 ;;;--------------------------< ex 4.36 >--------------------------
 
 
@@ -117,13 +125,54 @@
 ;;; 4.3.2 비결정적 프로그램 짜기
 ;;; p547
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; 논리 퍼즐
+(load "./amb-eval-likerivers12.scm")
+(driver-loop)
 
-;; (define (multiple-dwelling)
-;; ...
-;; )
+(define (require p)
+  (if (not p) (amb)))
 
 
+;;------------------
+;; amb 실행기에서 실행
+(define (distinct? list)
+  (define (find el lst)
+	 (cond
+	  ((null? lst) false)
+	  ((= el (car lst)) true)
+	  (else (find el (cdr lst)))))		  
+  (cond ((null? list) true)
+		((= 1 (length list)) true)
+		(else
+		 (if (find (car list) (cdr list))
+			 false
+			 (distinct? (cdr list))))))
+
+(define (multiple-dwelling)
+  (let ((baker (amb 1 2 3 4 5))
+        (cooper (amb 1 2 3 4 5))
+        (fletcher (amb 1 2 3 4 5))
+        (miller (amb 1 2 3 4 5))
+        (smith (amb 1 2 3 4 5)))
+    (require
+     (distinct? (list baker cooper fletcher miller smith)))
+    (require (not (= baker 5)))
+    (require (not (= cooper 1)))
+    (require (not (= fletcher 5)))
+    (require (not (= fletcher 1)))
+    (require (> miller cooper))
+    (require (not (= (abs (- smith fletcher)) 1)))
+    (require (not (= (abs (- fletcher cooper)) 1)))
+    (list (list 'baker baker)
+          (list 'cooper cooper)
+          (list 'fletcher fletcher)
+          (list 'miller miller)
+          (list 'smith smith))))
+
+(multiple-dwelling)
+;;->((baker 3) (cooper 2) (fletcher 4) (miller 5) (smith 1))
+try-again
 
 ;;;--------------------------< ex 4.38 >--------------------------
 
@@ -143,6 +192,8 @@
 ;;; 8퀸
 
 
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; 자연어 문법 분석
 ;;; p551
@@ -153,6 +204,9 @@
 (define (require p)
   (if (not p) (amb)))
 
+
+;;------------------
+;; amb 실행기에서 실행
 
 ;;Parsing natural language
 
@@ -189,6 +243,8 @@
 
 (parse '(the cat eats))
 ;;(sentence (noun-phrase (article the) (noun cat)) (verb eats))
+try-again
+
 
 (define prepositions '(prep for to in by with))
 
@@ -222,8 +278,11 @@
   (maybe-extend (parse-simple-noun-phrase)))
 
 (parse '(the student with the cat sleeps in the class))
+try-again
+
 
 (parse '(the professor lectures to the student with the cat))
+try-again
 
 
 ;;;--------------------------< ex 4.45 >--------------------------
@@ -243,233 +302,345 @@
 ;;; 4.3.3 amb 실행기 구현
 ;;; p547
 
-
-;;;;;;;;;;;;;;;;;
-;;; 실행기 프로시저와 앞으로 할 일
-
-
-;;;;;;;;;;;;;;;;;
-;;; 실행기의 구조
-;;; p560
-
-(define (amb? exp) (tagged-list? exp 'amb))
-
-(define (amb-choices exp) (cdr exp))
-
-
-;; analyze에 추가
-;;((amb? exp) (analyze-amb exp))
-
-(define (ambeval exp env succeed fail)
-  ((analyze exp) env succeed fail))
-
-;;;;;;;;;;;
-;;; 간단한 식
-;;; p562
-
-(define (analyze-self-evaluating exp)
-  (lambda (env succeed fail)
-    (succeed exp fail)))
-
-(define (analyze-quoted exp)
-  (let ((qval (text-of-quotation exp)))
-    (lambda (env succeed fail)
-      (succeed qval fail))))
-
-(define (analyze-variable exp)
-  (lambda (env succeed fail)
-    (succeed (lookup-variable-value exp env)
-	     fail)))
-
-
-(define (analyze-lambda exp)
-  (let ((vars (lambda-parameters exp))
-	(bproc (analyze-sequence (lambda-body exp))))
-    (lambda (env succeed fail)
-      (succeed (make-procedure vars bproc env)
-	       fail))))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;
-;;; 조건식과 차례식
-;;; p563
-
-(define (analyze-if exp)
-  (let ((pproc (analyze (if-predicate exp)))
-	(cproc (analyze (if-consequent exp)))
-	(aproc (analyze (if-alternative exp))))
-    (lambda (env succeed fail)
-      (pproc env
-	     ;; 성공했을 때 할 일
-	     (lambda (pred-value fail2)
-	       (if (true? pred-value)
-		   (cproc env succeed fail2)
-		   (aproc env succeed fail2)))
-	     ;; 실패했을 때 할 일
-	     fail))))
-
-(define (analyze-sequence exps)
-  (define (sequentially a b)
-    (lambda (env succeed fail)
-      (a env
-	 ;; 성공했을 때 호출할 함수
-	 (lambda (a-value fail2)
-	   (b env succeed fail2))
-	 ;; 실패했을 때 할 일
-	 fail)))
-  
-  (define (loop first-proc rest-procs)
-    (if (null? rest-procs)
-	first-proc
-	(loop (sequentially first-proc (car rest-procs))
-	      (cdr rest-procs))))
-  (let ((procs (map analyze exps)))
-    (if (null? procs)
-	(error "Empty sequence -- ANALYZE"))
-    (loop (car procs) (cdr procs))))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;
-;;; 정의와 덮어쓰기
-;;; p564
-
-(define (analyze-definition exp)
-  (let ((var (definition-variable exp))
-	(vproc (analyze (definition-value exp))))
-    (lambda (env succeed fail)
-      (vproc env
-	     (lambda (val fail2)
-	       (define-variable! var val env)
-	       (succeed 'ok fail2))
-	     fail))))
-
-(define (analyze-assignment exp)
-  (let ((var (assignment-variable exp))
-	(vproc (analyze (assignment-value exp))))
-    (lambda (env succeed fail)
-      (vproc env
-	     (lambda (val fail2)        ; *1*
-	       (let ((old-value
-		      (lookup-variable-value var env)))
-		 (set-variable-value! var val env)
-		 (succeed 'ok
-			  (lambda ()    ; *2*
-			    (set-variable-value! var
-						 old-value
-						 env)
-			    (fail2)))))
-	     fail))))
-
-
-;;;;;;;;;;;;;;;;;;;;;;
-;;; 프로시저 적용
-;;; p566
-(define (analyze-application exp)
-  (let ((fproc (analyze (operator exp)))
-	(aproc (map analyze (operands exp))))
-    (lambda (env succeed fail)
-      (fproc env 
-	     (lambda (proc fail2)
-	       (get-args aprocs
-			 env
-			 (lambda (args fail3)
-			   (execute-application
-			    proc args succeed fail3))
-			 fail2))
-	     fail))))
-
-(define (get-args aprocs env succeed fail)
-  (if (null? aprocs)
-      (succeed '() fail)
-      ((car aprocs) env
-       ;; 성공했을 때 aprocs를 위해 할 일
-       (lambda (arg fail2)
-	 (get-args (cdr aprocs)
-		   env
-		   ;; 성공했을 때 get-args를 위해 할 일
-		   (lambda (args fail3)
-		     (succeed (cons arg args)
-			      fail3))
-		   fail2))
-       fail)))
-
-
-(define (execute-application proc args succeed fail)
-  (cond ((primitive-procedure? proc)
-	 (succeed (apply-primitive-procedure proc args)
-		  fail))
-	((compound-procedure? proc)
-	 ((procedure-body proc)
-	  (extend-environment (procedure-parameters proc)
-			      args
-			      (procedure-environment proc))
-	  succeed
-	  fail))
-	(else
-	 (error
-	  "Unknown procedure type -- EXECUTE-APPLICATION"
-	  proc))))
-
-(define (analyze-amb exp)
-  (let ((cprocs (map analyze (amb-choices exp))))
-    (lambda (env succeed fail)
-      (define (try-next choices)
-	(if (null? choices)
-	    (fail)
-	    ((car choices) env
-	     succeed
-	     (lambda ()
-	       (try-next (cdr choices))))))
-      (try-next cprocs))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; 드라이버 루프
-;;; p569
-
-(define input-prompt ";;; Amb-Eval input:")
-
-(define output-prompt ";;; Amb-Eval value:")
-
-(define (driver-loop)
-  (define (internal-loop try-again)
-    (prompt-for-input input-prompt)
-    (let ((input (read)))
-      (if (eq? input 'try-again)
-	  (try-again)
-	  (begin
-	    (newline)
-	    (display ";;; Starting a new problem ")
-	    (ambeval input
-		     the-global-environment
-		     ;; ambeval의 성공한 다음 할 일
-		     (lambda (val next-alternative)
-		       (announce-output output-prompt)
-		       (user-print val)
-		       (internal-loop next-alternative))
-		     ;; ambeval의 실패한 다음 할 일
-		     (lambda ()
-		       (announce-output
-			";;; There are no more values of")
-		       (user-print input)
-		       (driver-loop)))))))
-  (internal-loop
-   (lambda ()
-     (newline)
-     (display ";; There is no current problem")
-     (driver-loop))))
-
-
-
-
-
+;;; 코드는 아래 파일에
+;;; (load "./amb-eval-likerivers12.scm")
 
 
 ;;;--------------------------< ex 4.50 >--------------------------
 
+
+(load "./amb-eval-likerivers12.scm")
+
+;;--------
+(define (ramb? exp) (tagged-list? exp 'ramb))
+(define (ramb-choices exp) (cdr exp))
+
+
+(define (analyze exp)
+  (cond ((self-evaluating? exp) 
+         (analyze-self-evaluating exp))
+        ((quoted? exp) (analyze-quoted exp))
+        ((variable? exp) (analyze-variable exp))
+        ((assignment? exp) (analyze-assignment exp))
+        ((definition? exp) (analyze-definition exp))
+        ((if? exp) (analyze-if exp))
+        ((lambda? exp) (analyze-lambda exp))
+        ((begin? exp) (analyze-sequence (begin-actions exp)))
+        ((cond? exp) (analyze (cond->if exp)))
+        ((let? exp) (analyze (let->combination exp))) ;**
+        ((amb? exp) (analyze-amb exp))                ;**
+		((ramb? exp) (analyze-ramb exp))
+        ((application? exp) (analyze-application exp))
+        (else
+         (error "Unknown expression type -- ANALYZE" exp))))
+
+
+(define (analyze-ramb exp)
+  (let ((cprocs (map analyze (amb-choices exp))))
+    (lambda (env succeed fail)
+      (define (try-next choices)
+        (if (null? choices)
+            (fail)
+			(let ((i (random (length choices))))
+			  (let ((choosen (list-ref choices i)))
+				(let ((rest (filter (lambda (c) (not (eq? c choosen))) choices)))
+				  (choosen env
+						   succeed
+						   (lambda ()
+							 (try-next rest))))))))
+	  (try-next cprocs))))
+
+;;-----
+(driver-loop)
+
+(define (require p)
+  (if (not p) (amb)))
+
+;;-
+(define (distinct? list)
+  (define (find el lst)
+	 (cond
+	  ((null? lst) false)
+	  ((= el (car lst)) true)
+	  (else (find el (cdr lst)))))		  
+  (cond ((null? list) true)
+		((= 1 (length list)) true)
+		(else
+		 (if (find (car list) (cdr list))
+			 false
+			 (distinct? (cdr list))))))
+
+(define (multiple-dwelling)
+  (let ((baker (amb 1 2 3 4 5))
+        (cooper (amb 1 2 3 4 5))
+        (fletcher (amb 1 2 3 4 5))
+        (miller (amb 1 2 3 4 5))
+        (smith (amb 1 2 3 4 5)))
+    (require
+     (distinct? (list baker cooper fletcher miller smith)))
+    (require (not (= baker 5)))
+    (require (not (= cooper 1)))
+    (require (not (= fletcher 5)))
+    (require (not (= fletcher 1)))
+    (require (> miller cooper))
+    (require (not (= (abs (- smith fletcher)) 1)))
+    (require (not (= (abs (- fletcher cooper)) 1)))
+    (list (list 'baker baker)
+          (list 'cooper cooper)
+          (list 'fletcher fletcher)
+          (list 'miller miller)
+          (list 'smith smith))))
+
+(multiple-dwelling)
+;;->((baker 3) (cooper 2) (fletcher 4) (miller 5) (smith 1))
+try-again
+
+
+
+
 ;;;--------------------------< ex 4.51 >--------------------------
+
+
+(load "./amb-eval-likerivers12.scm")
+
+;;--------
+
+(define (permanent-assignment? exp) (tagged-list? exp 'permanent-set!))
+(define (analyze exp)
+  (cond ((self-evaluating? exp) 
+         (analyze-self-evaluating exp))
+        ((quoted? exp) (analyze-quoted exp))
+        ((variable? exp) (analyze-variable exp))
+        ((assignment? exp) (analyze-assignment exp))
+		((permanent-assignment? exp) (analyze-permanent-assignment exp))
+        ((definition? exp) (analyze-definition exp))
+        ((if? exp) (analyze-if exp))
+        ((lambda? exp) (analyze-lambda exp))
+        ((begin? exp) (analyze-sequence (begin-actions exp)))
+        ((cond? exp) (analyze (cond->if exp)))
+        ((let? exp) (analyze (let->combination exp))) ;**
+        ((amb? exp) (analyze-amb exp))                ;**
+		((ramb? exp) (analyze-ramb exp))
+        ((application? exp) (analyze-application exp))
+        (else
+         (error "Unknown expression type -- ANALYZE" exp))))
+
+(define (analyze-permanent-assignment exp)
+  (let ((var (assignment-variable exp))
+        (vproc (analyze (assignment-value exp))))
+    (lambda (env succeed fail)
+      (vproc env
+             (lambda (val fail2)        ; *1*
+                 (set-variable-value! var val env)
+                 (succeed 'ok fail2))
+             fail))))
+
+;;----
+
+(driver-loop)
+
+(define (require p)
+  (if (not p) (amb)))
+
+(define (an-element-of items)
+  (require (not (null? items)))
+  (amb (car items) (an-element-of (cdr items))))
+
+(define count 0)
+
+(let ((x (an-element-of '(a b c)))
+	  (y (an-element-of '(a b c))))
+  (permanent-set! count (+ count 1))
+  (require (not (eq? x y)))
+  (list x y count))
+
+count
+
+(define count 0)
+
+(let ((x (an-element-of '(a b c)))
+	  (y (an-element-of '(a b c))))
+  (set! count (+ count 1))
+  (require (not (eq? x y)))
+  (list x y count))
+
+count
 
 ;;;--------------------------< ex 4.52 >--------------------------
 
+(load "./amb-eval-likerivers12.scm")
+
+;;--------
+
+(define (analyze exp)
+  (cond ((self-evaluating? exp) 
+         (analyze-self-evaluating exp))
+        ((quoted? exp) (analyze-quoted exp))
+        ((variable? exp) (analyze-variable exp))
+        ((assignment? exp) (analyze-assignment exp))
+		((permanent-assignment? exp) (analyze-permanent-assignment exp))
+        ((definition? exp) (analyze-definition exp))
+        ((if? exp) (analyze-if exp))
+		((if-fail? exp) (analyze-if-fail exp))
+        ((lambda? exp) (analyze-lambda exp))
+        ((begin? exp) (analyze-sequence (begin-actions exp)))
+        ((cond? exp) (analyze (cond->if exp)))
+        ((let? exp) (analyze (let->combination exp))) ;**
+        ((amb? exp) (analyze-amb exp))                ;**
+		((ramb? exp) (analyze-ramb exp))
+        ((application? exp) (analyze-application exp))
+        (else
+         (error "Unknown expression type -- ANALYZE" exp))))
+
+(define (if-fail? exp) (tagged-list? exp 'if-fail))
+
+(define (if-fail-proc exp) (cadr exp))
+(define (if-fail-onfailure exp) (caddr exp))
+
+  
+(define (analyze-if-fail exp)
+  (let ((proc (analyze (if-fail-proc exp)))
+        (fproc (analyze (if-fail-onfailure exp))))
+    (lambda (env succeed fail)
+	  (proc env
+			;; go ahead.
+			succeed
+			;; override failure contiuation.
+			(lambda ()
+			  (fproc env succeed fail))))))
+
+;;---
+(driver-loop)
+
+(define (require p)
+  (if (not p) (amb)))
+
+(define (an-element-of items)
+  (require (not (null? items)))
+  (amb (car items) (an-element-of (cdr items))))
+
+(if-fail (let ((x (an-element-of '(1 3 5))))
+		   (require (even? x))
+		   x)
+		 'all-odd)
+
+(if-fail (let ((x (an-element-of '(1 3 5 8))))
+		   (require (even? x))
+		   x)
+		 'all-odd)
+
 ;;;--------------------------< ex 4.53 >--------------------------
 
+;;; 4.52부터 수행
+
+(define (prime-sum-pair list1 list2)
+  (let ((a (an-element-of list1))
+		(b (an-element-of list2)))
+	(require (prime? (+ a b)))
+	(list a b)))
+
+(define (prime? x)
+  (define (iter n)
+	(cond ((= n x) true)
+		  ((= (remainder x n) 0) false)
+		  (else
+		   (iter (+ 1 n)))))
+  (if (< x 2)
+	  false
+	  (iter 2)))
+
+(let ((pairs '()))
+ (if-fail (let ((p (prime-sum-pair '(1 3 5 8) '(20 35 110))))
+			(permanent-set! pairs (cons p pairs))
+			(amb))
+		  pairs))
+
+
 ;;;--------------------------< ex 4.54 >--------------------------
+
+
+(load "./amb-eval-likerivers12.scm")
+
+;;--------
+
+
+(define (require? exp) (tagged-list? exp 'require))
+(define (require-predicate exp) (cadr exp))
+
+
+
+(define (analyze exp)
+  (cond ((self-evaluating? exp) 
+         (analyze-self-evaluating exp))
+        ((quoted? exp) (analyze-quoted exp))
+        ((variable? exp) (analyze-variable exp))
+        ((assignment? exp) (analyze-assignment exp))
+		((permanent-assignment? exp) (analyze-permanent-assignment exp))
+        ((definition? exp) (analyze-definition exp))
+        ((if? exp) (analyze-if exp))
+		((if-fail? exp) (analyze-if-fail exp))
+        ((lambda? exp) (analyze-lambda exp))
+        ((begin? exp) (analyze-sequence (begin-actions exp)))
+        ((cond? exp) (analyze (cond->if exp)))
+        ((let? exp) (analyze (let->combination exp))) ;**
+        ((amb? exp) (analyze-amb exp))                ;**
+		((ramb? exp) (analyze-ramb exp))
+		((require? exp) (analyze-require exp))
+        ((application? exp) (analyze-application exp))
+        (else
+         (error "Unknown expression type -- ANALYZE" exp))))
+
+(define (analyze-require exp)
+  (let ((pproc (analyze (require-predicate exp))))
+	(lambda (env succeed fail)
+	  (pproc env
+			 (lambda (pred-value fail2)
+			   (if (true? (not pred-value))
+				   (fail)
+				   (succeed 'ok fail2)))
+			 fail))))
+
+
+;;-----
+(driver-loop)
+
+;; require 정의없이
+
+;; test
+(define (distinct? list)
+  (define (find el lst)
+	 (cond
+	  ((null? lst) false)
+	  ((= el (car lst)) true)
+	  (else (find el (cdr lst)))))		  
+  (cond ((null? list) true)
+		((= 1 (length list)) true)
+		(else
+		 (if (find (car list) (cdr list))
+			 false
+			 (distinct? (cdr list))))))
+
+(define (multiple-dwelling)
+  (let ((baker (amb 1 2 3 4 5))
+        (cooper (amb 1 2 3 4 5))
+        (fletcher (amb 1 2 3 4 5))
+        (miller (amb 1 2 3 4 5))
+        (smith (amb 1 2 3 4 5)))
+    (require
+     (distinct? (list baker cooper fletcher miller smith)))
+    (require (not (= baker 5)))
+    (require (not (= cooper 1)))
+    (require (not (= fletcher 5)))
+    (require (not (= fletcher 1)))
+    (require (> miller cooper))
+    (require (not (= (abs (- smith fletcher)) 1)))
+    (require (not (= (abs (- fletcher cooper)) 1)))
+    (list (list 'baker baker)
+          (list 'cooper cooper)
+          (list 'fletcher fletcher)
+          (list 'miller miller)
+          (list 'smith smith))))
+
+(multiple-dwelling)
+;;->((baker 3) (cooper 2) (fletcher 4) (miller 5) (smith 1))
+try-again
