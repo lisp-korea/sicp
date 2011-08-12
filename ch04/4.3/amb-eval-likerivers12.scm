1476016ea77c6910031e261a380f20d325ca346c
suil son <likerivers12@gmail.com>
2011-08-12 22:35:47 +0900

    fix code

--- ch04/4.3/amb-eval-likerivers12.scm
+++ ch04/4.3/amb-eval-likerivers12.scm
@@ -62,201 +62,188 @@
 (define (ambeval exp env succeed fail)
   ((analyze exp) env succeed fail))
 
-;;;;;;;;;;;
-;;; 간단한 식
-;;; p562
+;;Simple expressions
 
 (define (analyze-self-evaluating exp)
   (lambda (env succeed fail)
     (succeed exp fail)))
-
 (define (analyze-quoted exp)
   (let ((qval (text-of-quotation exp)))
     (lambda (env succeed fail)
       (succeed qval fail))))
-
 (define (analyze-variable exp)
   (lambda (env succeed fail)
     (succeed (lookup-variable-value exp env)
-	     fail)))
-
-
+             fail)))
 (define (analyze-lambda exp)
   (let ((vars (lambda-parameters exp))
-	(bproc (analyze-sequence (lambda-body exp))))
+        (bproc (analyze-sequence (lambda-body exp))))
     (lambda (env succeed fail)
       (succeed (make-procedure vars bproc env)
-	       fail))))
+               fail))))
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;
-;;; 조건식과 차례식
-;;; p563
+;;Conditionals and sequences
 
 (define (analyze-if exp)
   (let ((pproc (analyze (if-predicate exp)))
-	(cproc (analyze (if-consequent exp)))
-	(aproc (analyze (if-alternative exp))))
+        (cproc (analyze (if-consequent exp)))
+        (aproc (analyze (if-alternative exp))))
     (lambda (env succeed fail)
       (pproc env
-	     ;; 성공했을 때 할 일
-	     (lambda (pred-value fail2)
-	       (if (true? pred-value)
-		   (cproc env succeed fail2)
-		   (aproc env succeed fail2)))
-	     ;; 실패했을 때 할 일
-	     fail))))
+             ;; success continuation for evaluating the predicate
+             ;; to obtain pred-value
+             (lambda (pred-value fail2)
+               (if (true? pred-value)
+                   (cproc env succeed fail2)
+                   (aproc env succeed fail2)))
+             ;; failure continuation for evaluating the predicate
+             fail))))
 
 (define (analyze-sequence exps)
   (define (sequentially a b)
     (lambda (env succeed fail)
       (a env
-	 ;; 성공했을 때 호출할 함수
-	 (lambda (a-value fail2)
-	   (b env succeed fail2))
-	 ;; 실패했을 때 할 일
-	 fail)))
-  
+         ;; success continuation for calling a
+         (lambda (a-value fail2)
+           (b env succeed fail2))
+         ;; failure continuation for calling a
+         fail)))
   (define (loop first-proc rest-procs)
     (if (null? rest-procs)
-	first-proc
-	(loop (sequentially first-proc (car rest-procs))
-	      (cdr rest-procs))))
+        first-proc
+        (loop (sequentially first-proc (car rest-procs))
+              (cdr rest-procs))))
   (let ((procs (map analyze exps)))
     (if (null? procs)
-	(error "Empty sequence -- ANALYZE"))
+        (error "Empty sequence -- ANALYZE"))
     (loop (car procs) (cdr procs))))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;
-;;; 정의와 덮어쓰기
-;;; p564
+;;Definitions and assignments
 
 (define (analyze-definition exp)
   (let ((var (definition-variable exp))
-	(vproc (analyze (definition-value exp))))
+        (vproc (analyze (definition-value exp))))
     (lambda (env succeed fail)
-      (vproc env
-	     (lambda (val fail2)
-	       (define-variable! var val env)
-	       (succeed 'ok fail2))
-	     fail))))
+      (vproc env                        
+             (lambda (val fail2)
+               (define-variable! var val env)
+               (succeed 'ok fail2))
+             fail))))
 
 (define (analyze-assignment exp)
   (let ((var (assignment-variable exp))
-	(vproc (analyze (assignment-value exp))))
+        (vproc (analyze (assignment-value exp))))
     (lambda (env succeed fail)
       (vproc env
-	     (lambda (val fail2)        ; *1*
-	       (let ((old-value
-		      (lookup-variable-value var env)))
-		 (set-variable-value! var val env)
-		 (succeed 'ok
-			  (lambda ()    ; *2*
-			    (set-variable-value! var
-						 old-value
-						 env)
-			    (fail2)))))
-	     fail))))
+             (lambda (val fail2)        ; *1*
+               (let ((old-value
+                      (lookup-variable-value var env))) 
+                 (set-variable-value! var val env)
+                 (succeed 'ok
+                          (lambda ()    ; *2*
+                            (set-variable-value! var
+                                                 old-value
+                                                 env)
+                            (fail2)))))
+             fail))))
 
 
-;;;;;;;;;;;;;;;;;;;;;;
-;;; 프로시저 적용
-;;; p566
+;;Procedure applications
+
 (define (analyze-application exp)
   (let ((fproc (analyze (operator exp)))
-	(aprocs (map analyze (operands exp))))
+        (aprocs (map analyze (operands exp))))
     (lambda (env succeed fail)
-      (fproc env 
-	     (lambda (proc fail2)
-	       (get-args aprocs
-			 env
-			 (lambda (args fail3)
-			   (execute-application
-			    proc args succeed fail3))
-			 fail2))
-	     fail))))
+      (fproc env
+             (lambda (proc fail2)
+               (get-args aprocs
+                         env
+                         (lambda (args fail3)
+                           (execute-application
+                            proc args succeed fail3))
+                         fail2))
+             fail))))
 
 (define (get-args aprocs env succeed fail)
   (if (null? aprocs)
       (succeed '() fail)
       ((car aprocs) env
-       ;; 성공했을 때 aprocs를 위해 할 일
-       (lambda (arg fail2)
-	 (get-args (cdr aprocs)
-		   env
-		   ;; 성공했을 때 get-args를 위해 할 일
-		   (lambda (args fail3)
-		     (succeed (cons arg args)
-			      fail3))
-		   fail2))
-       fail)))
-
+                    ;; success continuation for this aproc
+                    (lambda (arg fail2)
+                      (get-args (cdr aprocs)
+                                env
+                                ;; success continuation for recursive
+                                ;; call to get-args
+                                (lambda (args fail3)
+                                  (succeed (cons arg args)
+                                           fail3))
+                                fail2))
+                    fail)))
 
 (define (execute-application proc args succeed fail)
   (cond ((primitive-procedure? proc)
-	 (succeed (apply-primitive-procedure proc args)
-		  fail))
-	((compound-procedure? proc)
-	 ((procedure-body proc)
-	  (extend-environment (procedure-parameters proc)
-			      args
-			      (procedure-environment proc))
-	  succeed
-	  fail))
-	(else
-	 (error
-	  "Unknown procedure type -- EXECUTE-APPLICATION"
-	  proc))))
+         (succeed (apply-primitive-procedure proc args)
+                  fail))
+        ((compound-procedure? proc)
+         ((procedure-body proc)
+          (extend-environment (procedure-parameters proc)
+                              args
+                              (procedure-environment proc))
+          succeed
+          fail))
+        (else
+         (error
+          "Unknown procedure type -- EXECUTE-APPLICATION"
+          proc))))
+
+
+;;Evaluating amb expressions
 
 (define (analyze-amb exp)
   (let ((cprocs (map analyze (amb-choices exp))))
     (lambda (env succeed fail)
       (define (try-next choices)
-	(if (null? choices)
-	    (fail)
-	    ((car choices) env
-	     succeed
-	     (lambda ()
-	       (try-next (cdr choices))))))
+        (if (null? choices)
+            (fail)
+            ((car choices) env
+                           succeed
+                           (lambda ()
+                             (try-next (cdr choices))))))
       (try-next cprocs))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; 드라이버 루프
-;;; p569
+
+;;Driver loop
 
 (define input-prompt ";;; Amb-Eval input:")
-
 (define output-prompt ";;; Amb-Eval value:")
-
 (define (driver-loop)
   (define (internal-loop try-again)
     (prompt-for-input input-prompt)
     (let ((input (read)))
       (if (eq? input 'try-again)
-	  (try-again)
-	  (begin
-	    (newline)
-	    (display ";;; Starting a new problem ")
-	    (ambeval input
-		     the-global-environment
-		     ;; ambeval의 성공한 다음 할 일
-		     (lambda (val next-alternative)
-		       (announce-output output-prompt)
-		       (user-print val)
-		       (internal-loop next-alternative))
-		     ;; ambeval의 실패한 다음 할 일
-		     (lambda ()
-		       (announce-output
-			";;; There are no more values of")
-		       (user-print input)
-		       (driver-loop)))))))
+          (try-again)
+          (begin
+            (newline)
+            (display ";;; Starting a new problem ")
+            (ambeval input
+                     the-global-environment
+                     ;; ambeval success
+                     (lambda (val next-alternative)
+                       (announce-output output-prompt)
+                       (user-print val)
+                       (internal-loop next-alternative))
+                     ;; ambeval failure
+                     (lambda ()
+                       (announce-output
+                        ";;; There are no more values of")
+                       (user-print input)
+                       (driver-loop)))))))
   (internal-loop
    (lambda ()
      (newline)
-     (display ";; There is no current problem")
+     (display ";;; There is no current problem")
      (driver-loop))))
-
 
 
 
--- ch04/4.3/ex-4-3-likerivers12.scm
+++ ch04/4.3/ex-4-3-likerivers12.scm
@@ -147,32 +147,31 @@
 ;;; 자연어 문법 분석
 ;;; p551
 
+(load "./amb-eval-likerivers12.scm")
+(driver-loop)
 
-;; 낱말의 품사
+(define (require p)
+  (if (not p) (amb)))
+
+
+;;Parsing natural language
+
 (define nouns '(noun student professor cat class))
-
 (define verbs '(verb studies lectures eats sleeps))
-
 (define articles '(article the a))
 
-
-;; 각 문법 요소가 어떻게 더 단순한 요소로 짜맞추어지는지 밝히는 규칙
-;; (setence (noun-phrase (article the) (noun cat))
-;; 	 (verb eats))
-
-;.
-;.
-;.
+;;(sentence (noun-phrase (article the) (noun cat))
+;;          (verb eats))
 
 (define (parse-sentence)
   (list 'sentence
-	(parse-noun-phrase)
-	(parse-word verbs)))
+         (parse-noun-phrase)
+         (parse-word verbs)))
 
 (define (parse-noun-phrase)
   (list 'noun-phrase
-	(parse-word articles)
-	(parse-word nouns)))
+        (parse-word articles)
+        (parse-word nouns)))
 
 (define (parse-word word-list)
   (require (not (null? *unparsed*)))
@@ -181,53 +180,50 @@
     (set! *unparsed* (cdr *unparsed*))
     (list (car word-list) found-word)))
 
-
 (define *unparsed* '())
-
 (define (parse input)
   (set! *unparsed* input)
   (let ((sent (parse-sentence)))
     (require (null? *unparsed*))
     sent))
 
-;;
 (parse '(the cat eats))
-
-
+;;(sentence (noun-phrase (article the) (noun cat)) (verb eats))
 
 (define prepositions '(prep for to in by with))
 
-
 (define (parse-prepositional-phrase)
   (list 'prep-phrase
-	(parse-word prepositions)
-	(parse-noun-phrase)))
+        (parse-word prepositions)
+        (parse-noun-phrase)))
 
+(define (parse-sentence)
+  (list 'sentence
+         (parse-noun-phrase)
+         (parse-verb-phrase)))
 (define (parse-verb-phrase)
   (define (maybe-extend verb-phrase)
     (amb verb-phrase
-	 (maybe-extend (list 'verb-phrase
-			     verb-phrase
-			     (parse-prepositional-phrase)))))
+         (maybe-extend (list 'verb-phrase
+                             verb-phrase
+                             (parse-prepositional-phrase)))))
   (maybe-extend (parse-word verbs)))
 
-
-(define (parse-simple-noun-phrae)
+(define (parse-simple-noun-phrase)
   (list 'simple-noun-phrase
-	(parse-word articles)
-	(parse-word nouns)))
-
-
+        (parse-word articles)
+        (parse-word nouns)))
 (define (parse-noun-phrase)
   (define (maybe-extend noun-phrase)
     (amb noun-phrase
-	 (maybe-extend (list 'noun-phrase
-			     noun-phrase
-			     (parse-prepositional-phrase)))))
+         (maybe-extend (list 'noun-phrase
+                             noun-phrase
+                             (parse-prepositional-phrase)))))
   (maybe-extend (parse-simple-noun-phrase)))
 
-;;
 (parse '(the student with the cat sleeps in the class))
+
+(parse '(the professor lectures to the student with the cat))
 
 
 ;;;--------------------------< ex 4.45 >--------------------------
@@ -252,7 +248,6 @@
 ;;; 실행기 프로시저와 앞으로 할 일
 
 
-
 ;;;;;;;;;;;;;;;;;
 ;;; 실행기의 구조
 ;;; p560
@@ -264,7 +259,6 @@
 
 ;; analyze에 추가
 ;;((amb? exp) (analyze-amb exp))
-
 
 (define (ambeval exp env succeed fail)
   ((analyze exp) env succeed fail))
@@ -463,6 +457,11 @@
      (newline)
      (display ";; There is no current problem")
      (driver-loop))))
+
+
+
+
+
 
 
 ;;;--------------------------< ex 4.50 >--------------------------
