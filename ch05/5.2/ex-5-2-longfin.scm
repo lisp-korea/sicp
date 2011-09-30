5edc400c811f87bb343b21473f7f285946cc754a
longfin <longfinfunnel@gmail.com>
2011-10-01 02:36:21 +0900

    -Solve to ex 5.13

--- ch05/5.2/ex-5-2-longfin.scm
+++ ch05/5.2/ex-5-2-longfin.scm
@@ -606,6 +606,234 @@
 			  (advance-pc pc)))))))
 
 
+;; ex 5.12
 
-		
-			
+(define (make-new-machine-ex-5-12)
+  (let ((pc (make-register 'pc))
+		(flag (make-register 'flag))
+		(stack (make-stack))
+		(the-instruction-sequence '())
+
+		(instruction-log '())
+		(entry-point-log '())
+		(register-stack-log '())
+		(register-source-log '())
+	(let ((the-ops
+		   (list (list 'initialize-stack
+					   (lambda () (stack 'initialize)))))
+		  (register-table
+		   (list (list 'pc pc)
+				 (list 'flag flag))))
+	  (define (allocate-register name)
+		(if (assoc name register-table)
+			(error "Multiply defined register: " name)
+			(set! register-table
+				  (cons (list name (make-register name))
+						register-table)))
+		'register-allocated)
+	  (define (lookup-register name)
+		(let ((val (assoc name register-table)))
+		  (if val
+			  (cadr val)
+			  (error "Unknown register:" name))))
+	  (define (execute)
+		(let ((insts (get-contents pc)))
+		  (if (null? insts)
+			  'done
+			  (begin
+				((instruction-execution-proc (car insts)))
+				(execute)))))
+	  (define (dispatch message)
+		(cond ((eq? message 'start)
+			   (set-contents! pc the-instruction-sequence)
+			   (execute))
+			  ((eq? message 'install-instruction-sequence)
+			   (lambda (seq) (set! the-instruction-sequence seq)))
+			  ((eq? message 'allocate-register) allocate-register)
+			  ((eq? message 'get-register) lookup-register)
+			  ((eq? message 'install-operations)
+			   (lambda (ops)
+				 (set! the-ops (append the-ops ops))))
+			  ((eq? message 'stack) stack)
+			  ((eq? message 'operations) the-ops)
+
+			  ;; getting log
+			  ((eq? message 'get-instruction-log) instruction-log)
+			  ((eq? message 'get-entry-point-log) entry-point-log)
+			  ((eq? message 'get-register-stack-log) register-stack-log)
+			  ((eq? message 'get-register-source-log) register-source-log)
+			  
+			  ;; logging information
+			  ((eq? message 'log-instruction)
+			   (lambda (inst)
+				 (let ((entry (instruction-text inst)
+							  instruction-log))
+				   (if entry
+					   '()
+					   (set! instruction-log
+							 (cons (list (instruction-text inst) 'occuered)
+								   instruction-log))))))
+
+			  ((eq? message 'log-entry-point)
+			   (lambda (inst)
+				 (let ((reg (cadadr inst)))
+				   (let ((entry (assoc reg entry-point-log)))
+					 (if entry
+						 '()
+						 (set! entry-point-log
+							   (cons (list reg 'point)
+									 entry-point-log)))))))
+
+			  ((eq? message 'log-register-stack)
+			   (lambda (inst)
+				 (let ((reg (cadr inst)))
+				   (let ((entry (assoc reg register-stack-log)))
+					 (if entry
+						 '()
+						 (set! register-stack-log
+							   (cons (list reg 'stack)
+									 register-stack-log)))))))
+
+			  ((eq? message 'log-register-source)
+			   (lambda (inst)
+				 (let ((reg (cadr inst))
+					   (src (caddr inst)))
+				   (let ((key (cons reg src)))
+					 (let ((entry (assoc key register-source-log)))
+					   (if entry
+						   '()
+						   (set! register-source-log
+								 (cons (list key '())
+									   register-source-log))))))))
+			  (else (error "Unknown request -- MACHINE" message))))
+	  dispatch))))
+
+;; instruction
+(define (make-execution-procedure-ex-5-12 inst labels machine
+										  pc flag stack ops)
+  ((machine 'log-instruction) inst)
+  (cond ((eq? (car inst) 'assign)
+		 (make-assign inst machine labels ops pc))
+		((eq? (car inst) 'test)
+		 (make-test inst machine labels ops flag pc))
+		((eq? (car inst) 'branch)
+		 (make-branch inst machine labels flag pc))
+		((eq? (car inst) 'goto)
+		 (make-goto inst machine labels pc))
+		((eq? (car inst) 'save)
+		 (make-save inst machine stack pc))
+		((eq? (car inst) 'restore)
+		 (make-restore inst machine stack pc))
+		((eq? (car inst) 'perform)
+		 (make-perform inst machine labels ops pc))
+		(else (error "Unknown instruction type -- ASSEMBLE" inst))))
+
+
+  ;; assign instructions
+(define (make-assign-ex-5-12 inst machine labels operations pc)
+  (let ((target
+		 (get-register machine (assign-reg-name inst)))
+		(value-exp (assign-value-exp inst)))
+	(let ((value-proc
+		   (if (operation-exp? value-exp)
+			   (make-operation-exp
+				value-exp machine labels operations)
+			   (make-primitive-exp
+				(car value-exp) machine labels))))
+	  (lambda ()
+		(set-contents! target (value-proc))
+		((machine 'log-register-source) inst)
+		(advance-pc pc)))))
+;; goto		
+(define (make-goto-ex-5-12 inst machine labels pc)
+  (let ((dest (goto-dest inst)))
+	(cond ((label-exp? dest)
+		   (let ((insts
+				  (lookup-label labels
+								(label-exp-label dest))))
+			 (lambda () (set-contents! pc insts))))
+		  ((register-exp? dest)
+		   (let ((reg
+				  (get-register machine
+								(register-exp-reg dest))))
+			 (lambda ()
+			   ((machine 'log-entry-point) inst)
+			   (set-contents! pc (get-contents reg)))))
+		  (else
+		   (error "Bad GOTO instruction -- ASSEMBLE" inst)))))
+
+
+;; stack
+(define (make-save-ex-5-12 inst machine stack pc)
+  (let ((reg (get-register machine
+						   (stack-inst-reg-name inst))))
+	(lambda ()
+	  (push stack (get-contents reg))
+	  ((machine 'log-register-stack) inst)
+	  (advance-pc pc))))
+
+(define (make-restore-ex-5-12 inst machine stack pc)
+  (let ((reg (get-register machine
+						   (stack-inst-reg-name inst))))
+	(lambda ()
+	  (set-contents! reg (pop stack))
+	  ((machine 'log-register-stack) inst)
+	  (advance-pc pc))))
+
+
+;; ex 5.13
+
+
+(define (make-machine-ex-5-13 ops controller-text)
+  (let ((machine (make-new-machine)))
+	((machine 'install-operations) ops)
+	((machine 'install-instruction-sequence)
+	 (assemble controller-text machine))
+	machine))
+
+(define (make-new-machine-ex-5-13)
+  (let ((pc (make-register 'pc))
+		(flag (make-register 'flag))
+		(stack (make-stack))
+		(the-instruction-sequence '()))
+	(let ((the-ops
+		   (list (list 'initialize-stack
+					   (lambda () (stack 'initialize)))))
+		  (register-table
+		   (list (list 'pc pc)
+				 (list 'flag flag))))
+	  (define (allocate-register name)
+		(if (assoc name register-table)
+			(error "Multiply defined register: " name)
+			(set! register-table
+				  (cons (list name (make-register name))
+						register-table)))
+		'register-allocated)
+	  (define (lookup-register name)
+		(let ((val (assoc name register-table)))
+		  (if val
+			  (cadr val)
+			  (allocate-register name))))
+	  (define (execute)
+		(let ((insts (get-contents pc)))
+		  (if (null? insts)
+			  'done
+			  (begin
+				((instruction-execution-proc (car insts)))
+				(execute)))))
+	  (define (dispatch message)
+		(cond ((eq? message 'start)
+			   (set-contents! pc the-instruction-sequence)
+			   (execute))
+			  ((eq? message 'install-instruction-sequence)
+			   (lambda (seq) (set! the-instruction-sequence seq)))
+			  ((eq? message 'allocate-register) allocate-register)
+			  ((eq? message 'get-register) lookup-register)
+			  ((eq? message 'install-operations)
+			   (lambda (ops)
+				 (set! the-ops (append the-ops ops))))
+			  ((eq? message 'stack) stack)
+			  ((eq? message 'operations) the-ops)
+			  (else (error "Unknown request -- MACHINE" message))))
+	  dispatch)))
+
